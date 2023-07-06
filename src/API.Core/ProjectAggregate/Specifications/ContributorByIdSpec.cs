0a9b10cef406e52b6df24e6855dff5f731a3ab3b
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-07-06 23:08:40 +0200

    Create ASP.NET Clean Architecture Solution

---
+++ src/API.Core/ProjectAggregate/Specifications/ContributorByIdSpec.cs
@@ -0,0 +1,13 @@
+﻿using Ardalis.Specification;
+using API.Core.ContributorAggregate;
+
+namespace API.Core.ProjectAggregate.Specifications;
+
+public class ContributorByIdSpec : Specification<Contributor>, ISingleResultSpecification
+{
+  public ContributorByIdSpec(int contributorId)
+  {
+    Query
+        .Where(contributor => contributor.Id == contributorId);
+  }
+}
