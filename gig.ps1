bca55bc0973a8e33d07a92503bb1f71a527cda87
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-07-06 22:42:23 +0200

    Create gig.ps1

---
+++ gig.ps1
@@ -0,0 +1,18 @@
+#For PowerShell v3
+Function gig {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string[]]$list
+    )
+    $params = ($list | ForEach-Object { [uri]::EscapeDataString($_) }) -join ','
+    Invoke-WebRequest -Uri "https://www.toptal.com/developers/gitignore/api/$params" | Select-Object -ExpandProperty content | Out-File -FilePath $(Join-Path -Path $pwd -ChildPath '.gitignore') -Encoding ascii
+}
+
+
+gig -list visualstudio, visualstudiocode, rider, powershell, fsharp
+
+Add-Content -Path .\.gitignore -Value '# Repository Specific' -Encoding utf8
+Add-Content -Path .\.gitignore -Value 'src/API.Web/database.sqlite' -Encoding utf8
+Add-Content -Path .\.gitignore -Value 'src/API.Web/database.sqlite-shm' -Encoding utf8
+Add-Content -Path .\.gitignore -Value 'src/API.Web/database.sqlite-wal' -Encoding utf8
+Add-Content -Path .\.gitignore -Value 'src/API.Web/log*.txt' -Encoding utf8
