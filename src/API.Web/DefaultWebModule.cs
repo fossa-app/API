24960983b303a016794b797c35523ff9d646f7c0
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-07-25 00:34:18 +0200

    Update DefaultWebModule.cs

--- src/API.Web/DefaultWebModule.cs
+++ src/API.Web/DefaultWebModule.cs
@@ -1,4 +1,5 @@
 ﻿using Autofac;
+using Fossa.API.Core.Tenant;
 using Fossa.API.Core.User;
 using Fossa.API.Web.Claims;
 
@@ -11,6 +12,7 @@ public class DefaultWebModule : Module
     builder
       .RegisterType<ClaimsProvider>()
       .As<IUserIdProvider<Guid>>()
+      .As<ITenantIdProvider<Guid>>()
       .InstancePerLifetimeScope();
   }
 }
