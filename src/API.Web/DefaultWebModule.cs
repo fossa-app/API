ed624ae533f13200517accc72378192054061ff0
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-07-25 00:18:47 +0200

    Create DefaultWebModule.cs

---
+++ src/API.Web/DefaultWebModule.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using Fossa.API.Core.User;
+using Fossa.API.Web.Claims;
+
+namespace Fossa.API.Web;
+
+public class DefaultWebModule : Module
+{
+  protected override void Load(ContainerBuilder builder)
+  {
+    builder
+      .RegisterType<ClaimsProvider>()
+      .As<IUserIdProvider<Guid>>()
+      .InstancePerLifetimeScope();
+  }
+}
