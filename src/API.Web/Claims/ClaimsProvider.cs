5d85e7cdbc9291ac963d2cf9f4baa158db278bc1
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-07-25 00:18:35 +0200

    Create ClaimsProvider.cs

---
+++ src/API.Web/Claims/ClaimsProvider.cs
@@ -0,0 +1,47 @@
+﻿using System.Security.Claims;
+using Fossa.API.Core.User;
+using LanguageExt;
+using static LanguageExt.Prelude;
+
+namespace Fossa.API.Web.Claims;
+
+public class ClaimsProvider : IUserIdProvider<Guid>
+{
+  private readonly IHttpContextAccessor _httpContextAccessor;
+
+  public ClaimsProvider(IHttpContextAccessor httpContextAccessor)
+  {
+    _httpContextAccessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
+  }
+
+  public Option<Guid> FindUserId()
+  {
+    return FindFirstClaimValue(ClaimTypes.NameIdentifier, Guid.Parse);
+  }
+
+  public Guid GetUserId()
+  {
+    return FindUserId().IfNone(() => throw new ClaimNotFoundException());
+  }
+
+  private Option<T> FindFirstClaimValue<T>(
+    string type,
+    Func<string, T> parser)
+  {
+    if (parser is null)
+    {
+      throw new ArgumentNullException(nameof(parser));
+    }
+
+    var rawValue = FindFirstClaimValue(type);
+
+    return rawValue.Map(parser);
+  }
+
+  private Option<string> FindFirstClaimValue(string type)
+  {
+    var claim = _httpContextAccessor.HttpContext?.User?.FindFirst(type);
+
+    return Optional(claim).Map(x => x.Value);
+  }
+}
