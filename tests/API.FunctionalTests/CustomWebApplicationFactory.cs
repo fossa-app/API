098c11553a933d124a75d40e97b3b4e658fb9212
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-07-26 13:26:18 +0200

    Cleanup

--- tests/API.FunctionalTests/CustomWebApplicationFactory.cs
+++ tests/API.FunctionalTests/CustomWebApplicationFactory.cs
@@ -1,16 +1,20 @@
-﻿using Fossa.API.Infrastructure.Data;
-using Fossa.API.Web;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Fossa.API.FunctionalTests;
 
 public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProgram> where TProgram : class
 {
+  protected override void ConfigureWebHost(IWebHostBuilder builder)
+  {
+    builder
+        .ConfigureServices(services =>
+        {
+        });
+  }
+
   /// <summary>
   /// Overriding CreateHost to avoid creating a separate ServiceProvider per this thread:
   /// https://github.com/dotnet-architecture/eShopOnWeb/issues/465
@@ -30,54 +34,8 @@ public class CustomWebApplicationFactory<TProgram> : WebApplicationFactory<TProg
     // context (AppDbContext).
     using (var scope = serviceProvider.CreateScope())
     {
-      var scopedServices = scope.ServiceProvider;
-      var db = scopedServices.GetRequiredService<AppDbContext>();
-
-      var logger = scopedServices
-          .GetRequiredService<ILogger<CustomWebApplicationFactory<TProgram>>>();
-
-      // Ensure the database is created.
-      db.Database.EnsureCreated();
-
-      try
-      {
-#pragma warning disable VSTHRD002 // Avoid problematic synchronous waits
-        SeedData.PopulateTestDataAsync(db).GetAwaiter().GetResult();
-#pragma warning restore VSTHRD002 // Avoid problematic synchronous waits
-      }
-      catch (Exception ex)
-      {
-        logger.LogError(ex, "An error occurred seeding the " +
-                            "database with test messages. Error: {exceptionMessage}", ex.Message);
-      }
     }
 
     return host;
-  }
-
-  protected override void ConfigureWebHost(IWebHostBuilder builder)
-  {
-    builder
-        .ConfigureServices(services =>
-        {
-          // Remove the app's ApplicationDbContext registration.
-          var descriptor = services.SingleOrDefault(
-          d => d.ServiceType ==
-              typeof(DbContextOptions<AppDbContext>));
-
-          if (descriptor != null)
-          {
-            services.Remove(descriptor);
-          }
-
-          // This should be set for each individual test run
-          var inMemoryCollectionName = Guid.NewGuid().ToString();
-
-          // Add ApplicationDbContext using an in-memory database for testing.
-          services.AddDbContext<AppDbContext>(options =>
-          {
-            options.UseInMemoryDatabase(inMemoryCollectionName);
-          });
-        });
   }
 }
