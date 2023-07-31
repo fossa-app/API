30cbbc80e7b5efda31b41e46a4e36312493e816e
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-07-31 21:32:40 +0200

    Create ITenantEntity.cs

---
+++ src/API.Core/Tenant/ITenantEntity.cs
@@ -0,0 +1,11 @@
+﻿using TIKSN.Data;
+
+namespace Fossa.API.Core.Tenant;
+
+public interface ITenantEntity<TEntityIdentity, TTenantIdentity>
+  : IEntity<TEntityIdentity>
+  where TEntityIdentity : IEquatable<TEntityIdentity>
+  where TTenantIdentity : IEquatable<TTenantIdentity>
+{
+  TTenantIdentity TenantID { get; }
+}
