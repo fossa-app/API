1a2a3b0753743398d1b6a35344f6f866af556835
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-08-01 23:15:20 +0200

    Create CompanyMongoEntity.cs

---
+++ src/API.Persistence/Mongo/Entities/CompanyMongoEntity.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using TIKSN.Data;
+
+namespace Fossa.API.Persistence.Mongo.Entities;
+
+public class CompanyMongoEntity : IEntity<long>
+{
+  [BsonId]
+  public long ID { get; set; }
+
+  public string? Name { get; set; }
+
+  public Guid TenantID { get; set; }
+}
