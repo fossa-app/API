3b17633e845675b49beb9fbea0813b35b19ad60c
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-08-02 19:40:00 +0200

    Update MongoClientProvider.cs

--- src/API.Persistence/Mongo/Repositories/MongoClientProvider.cs
+++ src/API.Persistence/Mongo/Repositories/MongoClientProvider.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Bson;
+using MongoDB.Driver;
 using TIKSN.Data.Mongo;
 
 namespace Fossa.API.Persistence.Mongo.Repositories;
@@ -7,5 +9,14 @@ public class MongoClientProvider : MongoClientProviderBase
 {
   public MongoClientProvider(IConfiguration configuration) : base(configuration, "MongoDB")
   {
+  }
+
+  protected override void ConfigureClientSettings(MongoClientSettings mongoClientSettings)
+  {
+    base.ConfigureClientSettings(mongoClientSettings);
+
+#pragma warning disable CS0618 // Type or member is obsolete
+    mongoClientSettings.GuidRepresentation = GuidRepresentation.Standard;
+#pragma warning restore CS0618 // Type or member is obsolete
   }
 }
