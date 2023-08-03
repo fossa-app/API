398e23e641f5ceefc520d004b8a7be999be2a19a
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-08-04 00:10:57 +0200

    Update CompanyModificationCommandHandler.cs

--- src/API.Core/Messages/Commands/CompanyModificationCommandHandler.cs
+++ src/API.Core/Messages/Commands/CompanyModificationCommandHandler.cs
@@ -6,11 +6,14 @@ namespace Fossa.API.Core.Messages.Commands;
 
 public class CompanyModificationCommandHandler : IRequestHandler<CompanyModificationCommand>
 {
+  private readonly ICompanyQueryRepository _companyQueryRepository;
   private readonly ICompanyRepository _companyRepository;
 
   public CompanyModificationCommandHandler(
+    ICompanyQueryRepository companyQueryRepository,
     ICompanyRepository companyRepository)
   {
+    _companyQueryRepository = companyQueryRepository ?? throw new ArgumentNullException(nameof(companyQueryRepository));
     _companyRepository = companyRepository ?? throw new ArgumentNullException(nameof(companyRepository));
   }
 
@@ -18,7 +21,8 @@ public class CompanyModificationCommandHandler : IRequestHandler<CompanyModifica
     CompanyModificationCommand request,
     CancellationToken cancellationToken)
   {
-    CompanyEntity entity = new(request.ID, request.TenantID, request.Name);
+    CompanyEntity entity = await _companyQueryRepository.GetOrDefaultAsync(request.ID, cancellationToken).ConfigureAwait(false);
+    entity = entity with { Name = request.Name };
     await _companyRepository.UpdateAsync(entity, cancellationToken).ConfigureAwait(false);
   }
 }
