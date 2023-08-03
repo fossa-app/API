d5a541e426d9490ad1f47d9fa9e8306edf82c1d9
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-08-04 00:29:41 +0200

    Update CompanyMongoRepository.cs

--- src/API.Persistence/Mongo/Repositories/CompanyMongoRepository.cs
+++ src/API.Persistence/Mongo/Repositories/CompanyMongoRepository.cs
@@ -1,7 +1,9 @@
 ﻿using Fossa.API.Core;
 using Fossa.API.Persistence.Mongo.Entities;
+using LanguageExt;
 using MongoDB.Driver;
 using TIKSN.Data.Mongo;
+using static LanguageExt.Prelude;
 
 namespace Fossa.API.Persistence.Mongo.Repositories;
 
@@ -15,6 +17,16 @@ public class CompanyMongoRepository
           mongoDatabaseProvider,
           "Companies")
   {
+  }
+
+  public async Task<Option<CompanyMongoEntity>> FindByTenantIdAsync(Guid tenantId, CancellationToken cancellationToken)
+  {
+    var filter =
+      Builders<CompanyMongoEntity>.Filter.Eq(item => item.TenantID, tenantId);
+
+    var entity = await SingleOrDefaultAsync(filter, cancellationToken).ConfigureAwait(false);
+
+    return Optional(entity);
   }
 
   public async Task<CompanyMongoEntity> GetByTenantIdAsync(Guid tenantId, CancellationToken cancellationToken)
