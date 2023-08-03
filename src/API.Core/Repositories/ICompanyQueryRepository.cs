5248be0cbd9fdb4c8708b9ebcb2b07830ec244fe
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-08-04 00:28:59 +0200

    Update ICompanyQueryRepository.cs

--- src/API.Core/Repositories/ICompanyQueryRepository.cs
+++ src/API.Core/Repositories/ICompanyQueryRepository.cs
@@ -1,10 +1,15 @@
 ﻿using Fossa.API.Core.Entities;
+using LanguageExt;
 using TIKSN.Data;
 
 namespace Fossa.API.Core.Repositories;
 
 public interface ICompanyQueryRepository : IQueryRepository<CompanyEntity, long>
 {
+  Task<Option<CompanyEntity>> FindByTenantIdAsync(
+    Guid tenantId,
+    CancellationToken cancellationToken);
+
   Task<CompanyEntity> GetByTenantIdAsync(
     Guid tenantId,
     CancellationToken cancellationToken);
