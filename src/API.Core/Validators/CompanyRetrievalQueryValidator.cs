a76ed501f0c28f1390015536b7caee9be502c927
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-08-04 00:41:42 +0200

    Create CompanyRetrievalQueryValidator.cs

---
+++ src/API.Core/Validators/CompanyRetrievalQueryValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using Fossa.API.Core.Messages.Queries;
+
+namespace Fossa.API.Core.Validators;
+
+public class CompanyRetrievalQueryValidator : AbstractValidator<CompanyRetrievalQuery>
+{
+  public CompanyRetrievalQueryValidator()
+  {
+    RuleFor(x => x.TenantID).NotEmpty();
+  }
+}
