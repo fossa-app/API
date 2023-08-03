7d10a76461071dec41663ce2071530ee3eb4a76d
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-08-04 00:40:53 +0200

    Create CompanyDeletionCommandValidator.cs

---
+++ src/API.Core/Validators/CompanyDeletionCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using Fossa.API.Core.Messages.Commands;
+
+namespace Fossa.API.Core.Validators;
+
+public class CompanyDeletionCommandValidator : AbstractValidator<CompanyDeletionCommand>
+{
+  public CompanyDeletionCommandValidator()
+  {
+    RuleFor(x => x.ID).NotEmpty();
+    RuleFor(x => x.TenantID).NotEmpty();
+  }
+}
