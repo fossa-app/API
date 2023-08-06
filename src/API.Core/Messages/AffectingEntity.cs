6e9c0e437fe827f41d55f3a7f7e6adb1759659ee
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-08-06 19:15:25 +0200

    Create AffectingEntity.cs

---
+++ src/API.Core/Messages/AffectingEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace Fossa.API.Core.Messages;
+
+public record AffectingEntity<TEntityIdentity>(
+  Type EntityType,
+  TEntityIdentity EntityID)
+  where TEntityIdentity : IEquatable<TEntityIdentity>;
