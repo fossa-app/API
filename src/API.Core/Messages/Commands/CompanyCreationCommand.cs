d2daba75997bfb55baee93b953df8eec8c91d4e5
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-08-06 18:46:03 +0200

    Update CompanyCreationCommand.cs

--- src/API.Core/Messages/Commands/CompanyCreationCommand.cs
+++ src/API.Core/Messages/Commands/CompanyCreationCommand.cs
@@ -1,6 +1,12 @@
-﻿namespace Fossa.API.Core.Messages.Commands;
+﻿using Fossa.API.Core.Entities;
+
+namespace Fossa.API.Core.Messages.Commands;
 
 public record CompanyCreationCommand(
   Guid TenantID,
   string Name)
-  : ITenantCommand<Guid>;
+  : EntityTenantCommand<CompanyEntity, long, Guid>(TenantID)
+{
+  public override IEnumerable<long> AffectingTenantEntitiesIdentities
+    => Enumerable.Empty<long>();
+}
