7f27ea7709bb0626dc3eaa3207045bbe72e6b01f
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-08-06 23:27:04 +0200

    Create PublicAPITests.cs

---
+++ tests/API.UnitTests/PublicAPITests.cs
@@ -0,0 +1,16 @@
+﻿using Fossa.API.Core;
+using PublicApiGenerator;
+
+namespace Fossa.API.UnitTests;
+
+[UsesVerify]
+public class PublicAPITests
+{
+  [Fact]
+  public Task MyAssemblyHasNoPublicAPIChangesAsync()
+  {
+    var publicApi = typeof(DefaultCoreModule).Assembly.GeneratePublicApi();
+
+    return Verify(publicApi);
+  }
+}
