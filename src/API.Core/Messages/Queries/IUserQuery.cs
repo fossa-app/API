cdc6f7a6089190d4ed016a732772c09fd9866a9d
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-08-20 18:35:15 +0200

    Create IUserQuery.cs

---
+++ src/API.Core/Messages/Queries/IUserQuery.cs
@@ -0,0 +1,8 @@
+﻿namespace Fossa.API.Core.Messages.Queries;
+
+public interface IUserQuery<out TUserIdentity, out TResult>
+  : IQuery<TResult>
+  where TUserIdentity : IEquatable<TUserIdentity>
+{
+  TUserIdentity UserID { get; }
+}
