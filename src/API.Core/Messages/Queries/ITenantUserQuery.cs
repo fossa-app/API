222df10b5ce17dd5671b89799a7f7e89655e4960
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-08-20 18:35:22 +0200

    Update ITenantUserQuery.cs

--- src/API.Core/Messages/Queries/ITenantUserQuery.cs
+++ src/API.Core/Messages/Queries/ITenantUserQuery.cs
@@ -2,8 +2,9 @@
 
 public interface ITenantUserQuery<TEntityIdentity, out TUserIdentity, out TTenantIdentity, out TResult>
   : ITenantQuery<TEntityIdentity, TTenantIdentity, TResult>
+  , IUserQuery<TUserIdentity, TResult>
   where TEntityIdentity : IEquatable<TEntityIdentity>
   where TTenantIdentity : IEquatable<TTenantIdentity>
+  where TUserIdentity : IEquatable<TUserIdentity>
 {
-  TUserIdentity UserID { get; }
 }
