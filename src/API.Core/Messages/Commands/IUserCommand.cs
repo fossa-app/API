c6d58b87c48ad23bd6a5cbb86f7b1038e1e801b6
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-08-20 18:32:22 +0200

    Create IUserCommand.cs

---
+++ src/API.Core/Messages/Commands/IUserCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace Fossa.API.Core.Messages.Commands;
+
+public interface IUserCommand<out TUserIdentity>
+  : ICommand
+  where TUserIdentity : IEquatable<TUserIdentity>
+{
+  TUserIdentity UserID { get; }
+}
