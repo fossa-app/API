984b2949b132b00c2a3209d5db76efad6bc8ec16
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-09-04 20:15:01 +0200

    Create SystemPropertiesRepositoryAdapter.cs

---
+++ src/API.Persistence/Mongo/Repositories/SystemPropertiesRepositoryAdapter.cs
@@ -0,0 +1,27 @@
+﻿using Fossa.API.Core.Entities;
+using Fossa.API.Core.Repositories;
+using Fossa.API.Persistence.Mongo.Entities;
+using TIKSN.Data.Mongo;
+using TIKSN.Mapping;
+
+namespace Fossa.API.Persistence.Mongo.Repositories;
+
+public class SystemPropertiesRepositoryAdapter
+  : MongoRepositoryAdapter<SystemPropertiesEntity, long, SystemPropertiesMongoEntity, long>
+    , ISystemPropertiesRepository, ISystemPropertiesQueryRepository
+{
+  private readonly ISystemPropertiesMongoRepository _dataRepository;
+
+  public SystemPropertiesRepositoryAdapter(
+    IMapper<SystemPropertiesEntity, SystemPropertiesMongoEntity> domainEntityToDataEntityMapper,
+    IMapper<SystemPropertiesMongoEntity, SystemPropertiesEntity> dataEntityToDomainEntityMapper,
+    ISystemPropertiesMongoRepository dataRepository) : base(
+    domainEntityToDataEntityMapper,
+    dataEntityToDomainEntityMapper,
+    IdentityMapper<long>.Instance,
+    IdentityMapper<long>.Instance,
+    dataRepository)
+  {
+    _dataRepository = dataRepository ?? throw new ArgumentNullException(nameof(dataRepository));
+  }
+}
