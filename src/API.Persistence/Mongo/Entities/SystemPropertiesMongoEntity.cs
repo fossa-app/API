591a306db3b81e9f496bf3f22b759b181d015582
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-09-04 20:14:49 +0200

    Create SystemPropertiesMongoEntity.cs

---
+++ src/API.Persistence/Mongo/Entities/SystemPropertiesMongoEntity.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using TIKSN.Data;
+
+namespace Fossa.API.Persistence.Mongo.Entities;
+
+public class SystemPropertiesMongoEntity : IEntity<long>
+{
+  [BsonId] public long ID { get; set; }
+
+  public byte[]? SystemID { get; set; }
+}
