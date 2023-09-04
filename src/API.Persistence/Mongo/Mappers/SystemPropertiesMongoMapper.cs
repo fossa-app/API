392fb6a37772eaee76f14c6507697ff92ec6c104
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-09-04 20:14:52 +0200

    Create SystemPropertiesMongoMapper.cs

---
+++ src/API.Persistence/Mongo/Mappers/SystemPropertiesMongoMapper.cs
@@ -0,0 +1,20 @@
+﻿using Fossa.API.Core.Entities;
+using Fossa.API.Persistence.Mongo.Entities;
+using TIKSN.Mapping;
+
+namespace Fossa.API.Persistence.Mongo.Mappers;
+
+public class SystemPropertiesMongoMapper
+  : IMapper<SystemPropertiesMongoEntity, SystemPropertiesEntity>
+    , IMapper<SystemPropertiesEntity, SystemPropertiesMongoEntity>
+{
+  public SystemPropertiesEntity Map(SystemPropertiesMongoEntity source)
+  {
+    return new SystemPropertiesEntity(source.ID, new Ulid(source.SystemID));
+  }
+
+  public SystemPropertiesMongoEntity Map(SystemPropertiesEntity source)
+  {
+    return new SystemPropertiesMongoEntity { ID = source.ID, SystemID = source.SystemID.ToByteArray(), };
+  }
+}
