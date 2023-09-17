811fc3931d821dccb523a79bdb55d10f9d83757a
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-09-18 00:38:28 +0200

    Create EmployeeCreationCommand.cs

---
+++ src/API.Core/Messages/Commands/EmployeeCreationCommand.cs
@@ -0,0 +1,15 @@
+﻿using Fossa.API.Core.Entities;
+
+namespace Fossa.API.Core.Messages.Commands;
+
+public record EmployeeCreationCommand(
+    Guid TenantID,
+    Guid UserID,
+    string FirstName,
+    string LastName,
+    string FullName)
+  : EntityTenantCommand<CompanyEntity, long, Guid>(TenantID)
+{
+  public override IEnumerable<long> AffectingTenantEntitiesIdentities
+    => Enumerable.Empty<long>();
+}
