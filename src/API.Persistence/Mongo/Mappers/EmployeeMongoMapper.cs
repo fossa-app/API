a383465306b00389d92f474465e9f220b9ec66e2
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-09-18 00:39:04 +0200

    Create EmployeeMongoMapper.cs

---
+++ src/API.Persistence/Mongo/Mappers/EmployeeMongoMapper.cs
@@ -0,0 +1,36 @@
+﻿using Fossa.API.Core.Entities;
+using Fossa.API.Persistence.Mongo.Entities;
+using TIKSN.Mapping;
+
+namespace Fossa.API.Persistence.Mongo.Mappers;
+
+public class EmployeeMongoMapper
+  : IMapper<EmployeeMongoEntity, EmployeeEntity>
+  , IMapper<EmployeeEntity, EmployeeMongoEntity>
+{
+  public EmployeeEntity Map(EmployeeMongoEntity source)
+  {
+    return new(
+      source.ID,
+      source.TenantID,
+      source.UserID,
+      source.CompanyId,
+      source?.FirstName ?? string.Empty,
+      source?.LastName ?? string.Empty,
+      source?.FullName ?? string.Empty);
+  }
+
+  public EmployeeMongoEntity Map(EmployeeEntity source)
+  {
+    return new EmployeeMongoEntity
+    {
+      ID = source.ID,
+      TenantID = source.TenantID,
+      UserID = source.UserID,
+      CompanyId = source.CompanyId,
+      FirstName = source.FirstName,
+      LastName = source.LastName,
+      FullName = source.FullName,
+    };
+  }
+}
