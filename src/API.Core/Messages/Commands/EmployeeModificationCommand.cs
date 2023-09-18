b5ff6e2c5fd88fc72221779d0d2a7cd7c4bf3b63
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-09-18 16:08:50 +0200

    Create EmployeeModificationCommand.cs

---
+++ src/API.Core/Messages/Commands/EmployeeModificationCommand.cs
@@ -0,0 +1,17 @@
+﻿using Fossa.API.Core.Entities;
+using LanguageExt;
+
+namespace Fossa.API.Core.Messages.Commands;
+
+public record EmployeeModificationCommand(
+    long ID,
+    Guid TenantID,
+    Guid UserID,
+    string FirstName,
+    string LastName,
+    string FullName)
+  : EntityTenantCommand<CompanyEntity, long, Guid>(TenantID)
+{
+  public override IEnumerable<long> AffectingTenantEntitiesIdentities
+    => Prelude.Seq1(ID);
+}
