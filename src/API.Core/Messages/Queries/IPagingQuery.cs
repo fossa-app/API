008817c3aac0fe8030401f07a17ea7be5fa36ae0
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-09-18 21:48:47 +0200

    Create IPagingQuery.cs

---
+++ src/API.Core/Messages/Queries/IPagingQuery.cs
@@ -0,0 +1,11 @@
+﻿using TIKSN.Data;
+
+namespace Fossa.API.Core.Messages.Queries;
+
+public interface IPagingQuery<TEntity, TEntityIdentity>
+  : IQuery<PageResult<TEntity>>
+  where TEntity : IEntity<TEntityIdentity>
+  where TEntityIdentity : IEquatable<TEntityIdentity>
+{
+  Page Page { get; }
+}
