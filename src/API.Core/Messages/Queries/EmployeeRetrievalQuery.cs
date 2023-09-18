e0350bec60f98b219679865c1d75b67e8f22901d
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-09-18 16:44:23 +0200

    Create EmployeeRetrievalQuery.cs

---
+++ src/API.Core/Messages/Queries/EmployeeRetrievalQuery.cs
@@ -0,0 +1,12 @@
+﻿using Fossa.API.Core.Entities;
+
+namespace Fossa.API.Core.Messages.Queries;
+
+public record EmployeeRetrievalQuery(
+    Guid TenantID,
+    Guid UserID)
+  : EntityTenantQuery<EmployeeEntity, long, Guid, EmployeeEntity>(TenantID)
+{
+  public override IEnumerable<long> AffectingTenantEntitiesIdentities
+    => Enumerable.Empty<long>();
+}
