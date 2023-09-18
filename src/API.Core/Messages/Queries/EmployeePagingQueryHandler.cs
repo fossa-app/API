75bae686ae24c1510f03cf7de0a8caa91a5dd2a1
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-09-18 21:49:39 +0200

    Create EmployeePagingQueryHandler.cs

---
+++ src/API.Core/Messages/Queries/EmployeePagingQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using Fossa.API.Core.Entities;
+using Fossa.API.Core.Repositories;
+using MediatR;
+using TIKSN.Data;
+
+namespace Fossa.API.Core.Messages.Queries;
+
+public class EmployeePagingQueryHandler : IRequestHandler<EmployeePagingQuery, PageResult<EmployeeEntity>>
+{
+  private readonly IEmployeeQueryRepository _employeeQueryRepository;
+
+  public EmployeePagingQueryHandler(IEmployeeQueryRepository employeeQueryRepository)
+  {
+    _employeeQueryRepository =
+      employeeQueryRepository ?? throw new ArgumentNullException(nameof(employeeQueryRepository));
+  }
+
+  public Task<PageResult<EmployeeEntity>> Handle(
+    EmployeePagingQuery request,
+    CancellationToken cancellationToken)
+  {
+    return _employeeQueryRepository.PageAsync(
+      new TenantEmployeePageQuery(request.TenantID, request.Page),
+      cancellationToken);
+  }
+}
