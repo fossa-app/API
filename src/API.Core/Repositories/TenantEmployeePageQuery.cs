0b56fa5fddf77601e9f491acaccea8a952f4e15f
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-09-18 21:49:05 +0200

    Create TenantEmployeePageQuery.cs

---
+++ src/API.Core/Repositories/TenantEmployeePageQuery.cs
@@ -0,0 +1,15 @@
+﻿using TIKSN.Data;
+
+namespace Fossa.API.Core.Repositories;
+
+public class TenantEmployeePageQuery : PageQuery
+{
+  public Guid TenantId { get; }
+
+  public TenantEmployeePageQuery(
+    Guid tenantId,
+    Page page) : base(page, estimateTotalItems: true)
+  {
+    TenantId = tenantId;
+  }
+}
