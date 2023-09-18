9a76632490ed4344cbdfb7d684bb653e983b762b
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-09-18 16:14:14 +0200

    Create EmployeeModificationCommandValidator.cs

---
+++ src/API.Core/Validators/EmployeeModificationCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using Fossa.API.Core.Messages.Commands;
+
+namespace Fossa.API.Core.Validators;
+
+public class EmployeeModificationCommandValidator : AbstractValidator<EmployeeModificationCommand>
+{
+  public EmployeeModificationCommandValidator()
+  {
+    RuleFor(x => x.ID).NotEmpty();
+    RuleFor(x => x.TenantID).NotEmpty();
+    RuleFor(x => x.UserID).NotEmpty();
+    RuleFor(x => x.FirstName).NotEmpty();
+    RuleFor(x => x.LastName).NotEmpty();
+    RuleFor(x => x.FullName).NotEmpty();
+  }
+}
