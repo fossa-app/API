a0d3a3da328f6555686ee2bafe4e3338d5b96235
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-09-19 11:17:02 +0200

    Update IPagingQuery.cs

--- src/API.Core/Messages/Queries/IPagingQuery.cs
+++ src/API.Core/Messages/Queries/IPagingQuery.cs
@@ -2,10 +2,12 @@
 
 namespace Fossa.API.Core.Messages.Queries;
 
-public interface IPagingQuery<TEntity, TEntityIdentity>
-  : IQuery<PageResult<TEntity>>
-  where TEntity : IEntity<TEntityIdentity>
-  where TEntityIdentity : IEquatable<TEntityIdentity>
+public interface IPagingQuery
 {
   Page Page { get; }
+}
+
+public interface IPagingQuery<TEntity>
+  : IQuery<PageResult<TEntity>>, IPagingQuery
+{
 }
