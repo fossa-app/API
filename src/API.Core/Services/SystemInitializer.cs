666e1a8428ccaa3236d714ccf962cfa9ea6c2044
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-09-19 13:11:24 +0200

    Update SystemInitializer.cs

--- src/API.Core/Services/SystemInitializer.cs
+++ src/API.Core/Services/SystemInitializer.cs
@@ -5,14 +5,17 @@ namespace Fossa.API.Core.Services;
 public class SystemInitializer : ISystemInitializer
 {
   private readonly ISystemPropertiesInitializer _systemPropertiesInitializer;
+  private readonly ISystemLicenseInitializer _systemLicenseInitializer;
   private readonly ILogger<SystemInitializer> _logger;
 
   public SystemInitializer(
     ISystemPropertiesInitializer systemPropertiesInitializer,
+    ISystemLicenseInitializer systemLicenseInitializer,
     ILogger<SystemInitializer> logger)
   {
     _systemPropertiesInitializer = systemPropertiesInitializer ??
                                    throw new ArgumentNullException(nameof(systemPropertiesInitializer));
+    _systemLicenseInitializer = systemLicenseInitializer ?? throw new ArgumentNullException(nameof(systemLicenseInitializer));
     _logger = logger ?? throw new ArgumentNullException(nameof(logger));
   }
 
@@ -20,6 +23,7 @@ public class SystemInitializer : ISystemInitializer
   {
     _logger.LogInformation(2057718973, "System Initialization Started");
     await _systemPropertiesInitializer.InitializeAsync(cancellationToken).ConfigureAwait(false);
+    await _systemLicenseInitializer.InitializeAsync(cancellationToken).ConfigureAwait(false);
     _logger.LogInformation(2057718973, "System Initialization Finished");
   }
 }
