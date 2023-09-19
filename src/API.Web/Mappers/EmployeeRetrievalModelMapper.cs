987458104b4949655c260ee7beba8cdfa3f719f3
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-09-19 10:08:59 +0200

    Create EmployeeRetrievalModelMapper.cs

---
+++ src/API.Web/Mappers/EmployeeRetrievalModelMapper.cs
@@ -0,0 +1,15 @@
+﻿using Fossa.API.Core.Entities;
+using Fossa.API.Web.ApiModels;
+using TIKSN.Mapping;
+
+namespace Fossa.API.Web.Mappers;
+
+public class EmployeeRetrievalModelMapper : IMapper<EmployeeEntity, EmployeeRetrievalModel>
+{
+  public EmployeeRetrievalModel Map(EmployeeEntity source)
+  {
+    return new EmployeeRetrievalModel(
+      source.ID, source.CompanyId,
+      source.FirstName, source.LastName, source.FullName);
+  }
+}
