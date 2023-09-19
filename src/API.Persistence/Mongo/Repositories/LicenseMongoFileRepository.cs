a76743c048dfccca978bdaced2f2b4dba7dde9c2
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-09-19 13:11:03 +0200

    Create LicenseMongoFileRepository.cs

---
+++ src/API.Persistence/Mongo/Repositories/LicenseMongoFileRepository.cs
@@ -0,0 +1,12 @@
+﻿using Fossa.API.Core.Repositories;
+using TIKSN.Data.Mongo;
+
+namespace Fossa.API.Persistence.Mongo.Repositories;
+
+public class LicenseMongoFileRepository : MongoFileRepository<Guid, object>, ILicenseFileRepository
+{
+  public LicenseMongoFileRepository(IMongoDatabaseProvider mongoDatabaseProvider)
+    : base(mongoDatabaseProvider, "License")
+  {
+  }
+}
