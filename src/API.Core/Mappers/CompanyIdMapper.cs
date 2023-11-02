a7309558b5ef923c8c71f52d4a2d4fd3cfd0715f
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-11-02 18:20:56 +0100

    Create CompanyIdMapper.cs

---
+++ src/API.Core/Mappers/CompanyIdMapper.cs
@@ -0,0 +1,17 @@
+﻿using Fossa.API.Core.Entities;
+using TIKSN.Mapping;
+
+namespace Fossa.API.Core.Mappers;
+
+public class CompanyIdMapper : IMapper<CompanyId, long>, IMapper<long, CompanyId>
+{
+  public long Map(CompanyId source)
+  {
+    return source.AsPrimitive();
+  }
+
+  public CompanyId Map(long source)
+  {
+    return new CompanyId(source);
+  }
+}
