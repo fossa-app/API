2ecd4a46250d25ec758f85f76609c5baf43d5b44
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-11-03 15:32:08 +0100

    Update EmployeeMongoMapper.cs

--- src/API.Persistence/Mongo/Mappers/EmployeeMongoMapper.cs
+++ src/API.Persistence/Mongo/Mappers/EmployeeMongoMapper.cs
@@ -4,17 +4,31 @@ using TIKSN.Mapping;
 
 namespace Fossa.API.Persistence.Mongo.Mappers;
 
-public class EmployeeMongoMapper
-  : IMapper<EmployeeMongoEntity, EmployeeEntity>
-  , IMapper<EmployeeEntity, EmployeeMongoEntity>
+public class EmployeeMongoMapper : IMapper<EmployeeMongoEntity, EmployeeEntity>, IMapper<EmployeeEntity, EmployeeMongoEntity>
 {
+  private readonly IMapper<EmployeeId, long> _domainIdentityToDataIdentityMapper;
+  private readonly IMapper<long, EmployeeId> _dataIdentityToDomainIdentityMapper;
+  private readonly IMapper<CompanyId, long> _companyDomainIdentityToDataIdentityMapper;
+  private readonly IMapper<long, CompanyId> _companyDataIdentityToDomainIdentityMapper;
+
+  public EmployeeMongoMapper(
+    IMapper<EmployeeId, long> domainIdentityToDataIdentityMapper,
+    IMapper<long, EmployeeId> dataIdentityToDomainIdentityMapper,
+    IMapper<CompanyId, long> companyDomainIdentityToDataIdentityMapper,
+    IMapper<long, CompanyId> companyDataIdentityToDomainIdentityMapper)
+  {
+    _domainIdentityToDataIdentityMapper = domainIdentityToDataIdentityMapper ?? throw new ArgumentNullException(nameof(domainIdentityToDataIdentityMapper));
+    _dataIdentityToDomainIdentityMapper = dataIdentityToDomainIdentityMapper ?? throw new ArgumentNullException(nameof(dataIdentityToDomainIdentityMapper));
+    _companyDomainIdentityToDataIdentityMapper = companyDomainIdentityToDataIdentityMapper ?? throw new ArgumentNullException(nameof(companyDomainIdentityToDataIdentityMapper));
+    _companyDataIdentityToDomainIdentityMapper = companyDataIdentityToDomainIdentityMapper ?? throw new ArgumentNullException(nameof(companyDataIdentityToDomainIdentityMapper));
+  }
   public EmployeeEntity Map(EmployeeMongoEntity source)
   {
-    return new(
+    return new EmployeeEntity(
       source.ID,
       source.TenantID,
       source.UserID,
-      source.CompanyId,
+      _companyDataIdentityToDomainIdentityMapper.Map(source.CompanyId),
       source?.FirstName ?? string.Empty,
       source?.LastName ?? string.Empty,
       source?.FullName ?? string.Empty);
@@ -27,7 +41,7 @@ public class EmployeeMongoMapper
       ID = source.ID,
       TenantID = source.TenantID,
       UserID = source.UserID,
-      CompanyId = source.CompanyId,
+      CompanyId = _companyDomainIdentityToDataIdentityMapper.Map(source.CompanyId),
       FirstName = source.FirstName,
       LastName = source.LastName,
       FullName = source.FullName,
