1e976c9f1bf46cd656ffdbbb982bfcf1283f4fd5
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-11-03 15:31:58 +0100

    Update CompanyMongoMapper.cs

--- src/API.Persistence/Mongo/Mappers/CompanyMongoMapper.cs
+++ src/API.Persistence/Mongo/Mappers/CompanyMongoMapper.cs
@@ -4,20 +4,33 @@ using TIKSN.Mapping;
 
 namespace Fossa.API.Persistence.Mongo.Mappers;
 
-public class CompanyMongoMapper
-  : IMapper<CompanyMongoEntity, CompanyEntity>
-  , IMapper<CompanyEntity, CompanyMongoEntity>
+public class CompanyMongoMapper : IMapper<CompanyMongoEntity, CompanyEntity>, IMapper<CompanyEntity, CompanyMongoEntity>
 {
+  private readonly IMapper<CompanyId, long> _domainIdentityToDataIdentityMapper;
+  private readonly IMapper<long, CompanyId> _dataIdentityToDomainIdentityMapper;
+
+  public CompanyMongoMapper(
+    IMapper<CompanyId, long> domainIdentityToDataIdentityMapper,
+    IMapper<long, CompanyId> dataIdentityToDomainIdentityMapper)
+  {
+    _domainIdentityToDataIdentityMapper = domainIdentityToDataIdentityMapper ??
+                                          throw new ArgumentNullException(nameof(domainIdentityToDataIdentityMapper));
+    _dataIdentityToDomainIdentityMapper = dataIdentityToDomainIdentityMapper ??
+                                          throw new ArgumentNullException(nameof(dataIdentityToDomainIdentityMapper));
+  }
+
   public CompanyEntity Map(CompanyMongoEntity source)
   {
-    return new(source.ID, source.TenantID, source?.Name ?? string.Empty);
+    return new CompanyEntity(
+      _dataIdentityToDomainIdentityMapper.Map(source.ID),
+      source.TenantID, source?.Name ?? string.Empty);
   }
 
   public CompanyMongoEntity Map(CompanyEntity source)
   {
     return new CompanyMongoEntity
     {
-      ID = source.ID,
+      ID = _domainIdentityToDataIdentityMapper.Map(source.ID),
       TenantID = source.TenantID,
       Name = source.Name,
     };
