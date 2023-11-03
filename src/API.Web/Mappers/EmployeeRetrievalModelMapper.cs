1d0f0b8097e37b63e3b8c51eaf384420f20d5fb2
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-11-03 15:32:30 +0100

    Update EmployeeRetrievalModelMapper.cs

--- src/API.Web/Mappers/EmployeeRetrievalModelMapper.cs
+++ src/API.Web/Mappers/EmployeeRetrievalModelMapper.cs
@@ -6,10 +6,25 @@ namespace Fossa.API.Web.Mappers;
 
 public class EmployeeRetrievalModelMapper : IMapper<EmployeeEntity, EmployeeRetrievalModel>
 {
+  private readonly IMapper<CompanyId, long> _companyDomainIdentityToDataIdentityMapper;
+  private readonly IMapper<long, CompanyId> _companyDataIdentityToDomainIdentityMapper;
+
+  public EmployeeRetrievalModelMapper(
+    IMapper<CompanyId, long> companyDomainIdentityToDataIdentityMapper,
+    IMapper<long, CompanyId> companyDataIdentityToDomainIdentityMapper)
+  {
+    _companyDomainIdentityToDataIdentityMapper = companyDomainIdentityToDataIdentityMapper ??
+                                                 throw new ArgumentNullException(
+                                                   nameof(companyDomainIdentityToDataIdentityMapper));
+    _companyDataIdentityToDomainIdentityMapper = companyDataIdentityToDomainIdentityMapper ??
+                                                 throw new ArgumentNullException(
+                                                   nameof(companyDataIdentityToDomainIdentityMapper));
+  }
+
   public EmployeeRetrievalModel Map(EmployeeEntity source)
   {
     return new EmployeeRetrievalModel(
-      source.ID, source.CompanyId,
+      source.ID, _companyDomainIdentityToDataIdentityMapper.Map(source.CompanyId),
       source.FirstName, source.LastName, source.FullName);
   }
 }
