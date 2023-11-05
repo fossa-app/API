7dcd1fdcfcdd6a524be4e8f7fa4021d041d945a0
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-11-05 02:39:52 +0100

    Update PublicAPITests.cs

--- tests/API.UnitTests/PublicAPITests.cs
+++ tests/API.UnitTests/PublicAPITests.cs
@@ -1,4 +1,7 @@
 ﻿using Fossa.API.Core;
+using Fossa.API.Infrastructure;
+using Fossa.API.Persistence;
+using Fossa.API.Web;
 using PublicApiGenerator;
 
 namespace Fossa.API.UnitTests;
@@ -6,10 +9,36 @@ namespace Fossa.API.UnitTests;
 [UsesVerify]
 public class PublicAPITests
 {
+  private readonly ApiGeneratorOptions _apiGeneratorOptions = new() { IncludeAssemblyAttributes = false };
+
   [Fact]
-  public Task MyAssemblyHasNoPublicAPIChangesAsync()
+  public Task CoreAssemblyHasNoPublicAPIChangesAsync()
   {
-    var publicApi = typeof(DefaultCoreModule).Assembly.GeneratePublicApi();
+    var publicApi = typeof(DefaultCoreModule).Assembly.GeneratePublicApi(_apiGeneratorOptions);
+
+    return Verify(publicApi);
+  }
+
+  [Fact]
+  public Task InfrastructureAssemblyHasNoPublicAPIChangesAsync()
+  {
+    var publicApi = typeof(DefaultInfrastructureModule).Assembly.GeneratePublicApi(_apiGeneratorOptions);
+
+    return Verify(publicApi);
+  }
+
+  [Fact]
+  public Task PersistenceAssemblyHasNoPublicAPIChangesAsync()
+  {
+    var publicApi = typeof(DefaultPersistenceModule).Assembly.GeneratePublicApi(_apiGeneratorOptions);
+
+    return Verify(publicApi);
+  }
+
+  [Fact]
+  public Task WebAssemblyHasNoPublicAPIChangesAsync()
+  {
+    var publicApi = typeof(DefaultWebModule).Assembly.GeneratePublicApi(_apiGeneratorOptions);
 
     return Verify(publicApi);
   }
