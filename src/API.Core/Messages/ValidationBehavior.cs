9541c388884518dd11b73f4ee32dad5095e7ec57
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-11-11 20:22:12 +0100

    Update ValidationBehavior.cs

--- src/API.Core/Messages/ValidationBehavior.cs
+++ src/API.Core/Messages/ValidationBehavior.cs
@@ -1,15 +1,21 @@
 ﻿using FluentValidation;
+using LanguageExt;
 using MediatR;
 
 namespace Fossa.API.Core.Messages;
 
-public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse>
+public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+  where TRequest : IRequest<TResponse>
 {
-  private readonly IEnumerable<IValidator<TRequest>> _validators;
+  private readonly Seq<IValidator<TRequest>> _requestValidators;
+  private readonly Seq<IValidator<TResponse>> _responseValidators;
 
-  public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+  public ValidationBehavior(
+    IEnumerable<IValidator<TRequest>> requestValidators,
+    IEnumerable<IValidator<TResponse>> responseValidators)
   {
-    _validators = validators;
+    _requestValidators = requestValidators?.ToSeq() ?? throw new ArgumentNullException(nameof(requestValidators));
+    _responseValidators = responseValidators?.ToSeq() ?? throw new ArgumentNullException(nameof(responseValidators));
   }
 
   public async Task<TResponse> Handle(
@@ -17,15 +23,30 @@ public class ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TReques
     RequestHandlerDelegate<TResponse> next,
     CancellationToken cancellationToken)
   {
-    if (_validators.Any())
+    await EnsureValidityAsync(_requestValidators, request, cancellationToken).ConfigureAwait(false);
+
+    var response = await next().ConfigureAwait(false);
+
+    await EnsureValidityAsync(_responseValidators, response, cancellationToken).ConfigureAwait(false);
+
+    return response;
+  }
+
+  private static async Task EnsureValidityAsync<T>(
+    Seq<IValidator<T>> validators,
+    T instanceToValidate,
+    CancellationToken cancellationToken)
+  {
+    if (validators.Any())
     {
-      var context = new ValidationContext<TRequest>(request);
-      var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken))).ConfigureAwait(false);
+      var context = new ValidationContext<T>(instanceToValidate);
+      var validationResults = await Task
+        .WhenAll(validators.Select(v => v.ValidateAsync(context, cancellationToken))).ConfigureAwait(false);
       var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
       if (failures.Count != 0)
+      {
         throw new ValidationException(failures);
+      }
     }
-
-    return await next().ConfigureAwait(false);
   }
 }
