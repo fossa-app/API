454c56708bed46eb2ab1e06b4b417268fd7e389f
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-11-12 19:35:11 +0100

    Update BaseApiController.cs

--- src/API.Web/Api/BaseApiController.cs
+++ src/API.Web/Api/BaseApiController.cs
@@ -6,27 +6,36 @@ using TIKSN.Mapping;
 
 namespace Fossa.API.Web.Api;
 
-/// <summary>
-/// If your API controllers will use a consistent route convention and the [ApiController] attribute (they should)
-/// then it's a good idea to define and use a common base controller class like this one.
-/// </summary>
 [Route("api/[controller]")]
 [ApiController]
-public abstract class BaseApiController<TEntityIdentity> : Controller
+public abstract class BaseApiController : Controller
 {
   protected readonly IPublisher _publisher;
-  protected readonly IMapper<TEntityIdentity, long> _domainIdentityToDataIdentityMapper;
-  protected readonly IMapper<long, TEntityIdentity> _dataIdentityToDomainIdentityMapper;
   protected readonly ISender _sender;
+
+  protected BaseApiController(
+    ISender sender,
+    IPublisher publisher)
+  {
+    _sender = sender ?? throw new ArgumentNullException(nameof(sender));
+    _publisher = publisher ?? throw new ArgumentNullException(nameof(publisher));
+  }
+}
+
+[Route("api/[controller]")]
+[ApiController]
+public abstract class BaseApiController<TEntityIdentity> : BaseApiController
+{
+  protected readonly IMapper<long, TEntityIdentity> _dataIdentityToDomainIdentityMapper;
+  protected readonly IMapper<TEntityIdentity, long> _domainIdentityToDataIdentityMapper;
 
   protected BaseApiController(
     ISender sender,
     IPublisher publisher,
     IMapper<TEntityIdentity, long> domainIdentityToDataIdentityMapper,
     IMapper<long, TEntityIdentity> dataIdentityToDomainIdentityMapper)
+    : base(sender, publisher)
   {
-    _sender = sender ?? throw new ArgumentNullException(nameof(sender));
-    _publisher = publisher ?? throw new ArgumentNullException(nameof(publisher));
     _domainIdentityToDataIdentityMapper = domainIdentityToDataIdentityMapper ??
                                           throw new ArgumentNullException(nameof(domainIdentityToDataIdentityMapper));
     _dataIdentityToDomainIdentityMapper = dataIdentityToDomainIdentityMapper ??
