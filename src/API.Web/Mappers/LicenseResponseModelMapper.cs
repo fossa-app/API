cf6aeb880c0a5060cde066b9ed15ba9542dadc5f
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-11-13 22:44:12 +0100

    Create LicenseResponseModelMapper.cs

---
+++ src/API.Web/Mappers/LicenseResponseModelMapper.cs
@@ -0,0 +1,27 @@
+﻿using Fossa.API.Web.ApiModels;
+using TIKSN.Licensing;
+using TIKSN.Mapping;
+
+namespace Fossa.API.Web.Mappers;
+
+public class LicenseResponseModelMapper<TEntitlements, TEntitlementsModel>
+  : IMapper<License<TEntitlements>, LicenseResponseModel<TEntitlementsModel>>
+{
+  private readonly IMapper<TEntitlements, TEntitlementsModel> _entitlementsMapper;
+  private readonly IMapper<LicenseTerms, LicenseTermsModel> _licenseTermMapper;
+
+  public LicenseResponseModelMapper(
+    IMapper<TEntitlements, TEntitlementsModel> entitlementsMapper,
+    IMapper<LicenseTerms, LicenseTermsModel> licenseTermMapper)
+  {
+    _entitlementsMapper = entitlementsMapper ?? throw new ArgumentNullException(nameof(entitlementsMapper));
+    _licenseTermMapper = licenseTermMapper ?? throw new ArgumentNullException(nameof(licenseTermMapper));
+  }
+
+  public LicenseResponseModel<TEntitlementsModel> Map(License<TEntitlements> source)
+  {
+    return new LicenseResponseModel<TEntitlementsModel>(
+      _licenseTermMapper.Map(source.Terms),
+      _entitlementsMapper.Map(source.Entitlements));
+  }
+}
