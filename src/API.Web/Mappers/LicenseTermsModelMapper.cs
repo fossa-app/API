26cd4f3507d20d4d0391e413da6a14e06f2b6db7
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-11-13 22:44:01 +0100

    Create LicenseTermsModelMapper.cs

---
+++ src/API.Web/Mappers/LicenseTermsModelMapper.cs
@@ -0,0 +1,23 @@
+﻿using TIKSN.Licensing;
+using TIKSN.Mapping;
+
+namespace Fossa.API.Web.Mappers;
+
+public class LicenseTermsModelMapper : IMapper<LicenseTerms, LicenseTermsModel>
+{
+  private readonly IMapper<Party, PartyModel> _partyModelMapper;
+
+  public LicenseTermsModelMapper(IMapper<Party, PartyModel> partyModelMapper)
+  {
+    _partyModelMapper = partyModelMapper ?? throw new ArgumentNullException(nameof(partyModelMapper));
+  }
+
+  public LicenseTermsModel Map(LicenseTerms source)
+  {
+    return new LicenseTermsModel(
+      _partyModelMapper.Map(source.Licensor),
+      _partyModelMapper.Map(source.Licensee),
+      source.NotBefore,
+      source.NotAfter);
+  }
+}
