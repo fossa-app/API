6950ec5977ad3bd29eec90d6d168370bd5c8a561
Tigran TIKSN Torosyan <personal@tiksn.com>
2023-11-13 20:47:33 +0100

    Create CertificateProvider.cs

---
+++ src/API.Core/Services/CertificateProvider.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Fossa.API.Core.Properties;
+
+namespace Fossa.API.Core.Services;
+
+public class CertificateProvider : ICertificateProvider
+{
+  public Task<X509Certificate2> GetCertificateAsync(CancellationToken cancellationToken)
+  {
+    var certificate = new X509Certificate2(Resources.FossaFirstPartyLicensing);
+    return Task.FromResult(certificate);
+  }
+}
