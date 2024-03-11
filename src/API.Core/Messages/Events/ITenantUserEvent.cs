767948cdd3f944f632b916fdd7d109cff195898e
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-03-11 21:32:38 +0100

    Update ITenantUserEvent.cs

--- src/API.Core/Messages/Events/ITenantUserEvent.cs
+++ src/API.Core/Messages/Events/ITenantUserEvent.cs
@@ -3,7 +3,5 @@
 public interface ITenantUserEvent<out TUserIdentity, out TTenantIdentity>
   : ITenantEvent<TTenantIdentity>
   , IUserEvent<TUserIdentity>
-  where TTenantIdentity : IEquatable<TTenantIdentity>
   where TUserIdentity : IEquatable<TUserIdentity>
-{
-}
+  where TTenantIdentity : IEquatable<TTenantIdentity>;
