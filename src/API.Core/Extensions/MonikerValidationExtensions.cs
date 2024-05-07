b3f66ccf32b0e7e156643e894539bdd3d0b85bde
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-05-07 02:16:26 +0200

    Create MonikerValidationExtensions.cs

---
+++ src/API.Core/Extensions/MonikerValidationExtensions.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace Fossa.API.Core.Extensions;
+
+public static class MonikerValidationExtensions
+{
+  public static IRuleBuilderOptions<T, string> SubDomainLength<T>(this IRuleBuilder<T, string> ruleBuilder) =>
+    ruleBuilder
+      .Length(3, 63);
+
+  public static IRuleBuilderOptions<T, string> OnlyAsciiLetterLowersAndDigits<T>(this IRuleBuilder<T, string> ruleBuilder) =>
+    ruleBuilder
+      .Must((_, moniker, _) => moniker.ForAll(c => char.IsAsciiLetterLower(c) || char.IsAsciiDigit(c)))
+      .WithMessage("{PropertyName} must contain only ASCII lower letters or ASCII digits.");
+}
