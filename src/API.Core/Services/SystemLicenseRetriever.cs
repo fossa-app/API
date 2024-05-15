a2c1744aa2d3301eca7c551ab4a5998b252e13c4
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-05-16 00:28:52 +0200

    Update SystemLicenseRetriever.cs

--- src/API.Core/Services/SystemLicenseRetriever.cs
+++ src/API.Core/Services/SystemLicenseRetriever.cs
@@ -9,17 +9,20 @@ namespace Fossa.API.Core.Services;
 public class SystemLicenseRetriever : ISystemLicenseRetriever
 {
   private readonly ICertificateProvider _certificateProvider;
+  private readonly ISystemPropertiesQueryRepository _systemPropertiesQueryRepository;
   private readonly ILicenseFactory<SystemEntitlements, SystemLicenseEntitlements> _licenseFactory;
   private readonly ILicenseFileRepository _licenseFileRepository;
 
   public SystemLicenseRetriever(
     ILicenseFileRepository licenseFileRepository,
     ILicenseFactory<SystemEntitlements, SystemLicenseEntitlements> licenseFactory,
-    ICertificateProvider certificateProvider)
+    ICertificateProvider certificateProvider,
+    ISystemPropertiesQueryRepository systemPropertiesQueryRepository)
   {
     _licenseFileRepository = licenseFileRepository ?? throw new ArgumentNullException(nameof(licenseFileRepository));
     _licenseFactory = licenseFactory ?? throw new ArgumentNullException(nameof(licenseFactory));
     _certificateProvider = certificateProvider ?? throw new ArgumentNullException(nameof(certificateProvider));
+    _systemPropertiesQueryRepository = systemPropertiesQueryRepository ?? throw new ArgumentNullException(nameof(systemPropertiesQueryRepository));
   }
 
   public async Task<Validation<Error, License<SystemEntitlements>>> GetAsync(CancellationToken cancellationToken)
@@ -27,12 +30,17 @@ public class SystemLicenseRetriever : ISystemLicenseRetriever
     var licenseFile = await _licenseFileRepository.DownloadAsync(LicensePaths.SystemLicensePath, cancellationToken)
       .ConfigureAwait(false);
 
+    var systemPropertiesEntity = await _systemPropertiesQueryRepository.GetAsync(SystemProperties.MainSystemPropertiesId, cancellationToken).ConfigureAwait(false);
+
     var licenseData = licenseFile.Content.ToSeq();
 
     var certificate = await _certificateProvider.GetCertificateAsync(cancellationToken).ConfigureAwait(false);
 
     var licenseValidation = _licenseFactory.Create(licenseData, certificate);
 
-    return licenseValidation;
+    return licenseValidation
+      .Validate(
+        license => license.Entitlements.SystemId == systemPropertiesEntity.SystemID,
+        11751858, "Current System License is issued to another system.");
   }
 }
