bd4e896cb857267a2dd491fdba0f759635276c0f
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-05-15 03:36:46 +0200

    Create LicenseControllerEmptyDatabase.cs

---
+++ tests/API.FunctionalTests/ControllerApis/LicenseControllerEmptyDatabase.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using Fossa.API.Web;
+
+namespace Fossa.API.FunctionalTests.ControllerApis;
+
+[Collection("Sequential")]
+public class LicenseControllerEmptyDatabase : IClassFixture<CustomWebApplicationFactory<DefaultWebModule>>
+{
+  private readonly HttpClient _client;
+
+  public LicenseControllerEmptyDatabase(CustomWebApplicationFactory<DefaultWebModule> factory)
+  {
+    _client = factory.CreateClient();
+  }
+
+  [Fact]
+  public async Task RetrieveSystemLicenseAsync()
+  {
+    var response = await _client.GetAsync("/api/1.0/License/System");
+
+    Assert.Equal(HttpStatusCode.UnprocessableEntity, response.StatusCode);
+  }
+}
