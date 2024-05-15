3d7f7c52b62f32c6af9632f9a3f55bd983b6d09f
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-05-15 04:08:01 +0200

    Create ITestCertificateProvider.cs

---
+++ tests/API.FunctionalTests/Services/ITestCertificateProvider.cs
@@ -0,0 +1,8 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Fossa.API.FunctionalTests.Services;
+
+public interface ITestCertificateProvider
+{
+  X509Certificate2 Certificate { get; }
+}
