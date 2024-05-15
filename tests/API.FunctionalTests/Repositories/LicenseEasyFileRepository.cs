522ccc93bf06d2b7f20ebfd1c66b8af974e4311b
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-05-15 03:14:53 +0200

    Create LicenseEasyFileRepository.cs

---
+++ tests/API.FunctionalTests/Repositories/LicenseEasyFileRepository.cs
@@ -0,0 +1,15 @@
+﻿using EasyDoubles;
+using Fossa.API.Core.Repositories;
+using TIKSN.Identity;
+
+namespace Fossa.API.FunctionalTests.Repositories;
+
+public class LicenseEasyFileRepository : EasyFileRepository<long, object>, ILicenseFileRepository
+{
+  public LicenseEasyFileRepository(
+    IEasyStores easyStores,
+    IIdentityGenerator<long> identityGenerator)
+    : base(easyStores, identityGenerator, "License")
+  {
+  }
+}
