91d1193695787641ef0f813672b77647f6231891
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-06-22 16:55:15 +0200

    Create .build.ps1

---
+++ .build.ps1
@@ -0,0 +1,81 @@
+<#
+.Synopsis
+    Build script
+
+.Description
+    TASKS AND REQUIREMENTS
+    Initialize and Clean repository
+    Restore packages, workflows, tools
+    Format code
+    Build projects and the solution
+    Run Tests
+    Pack
+    Publish
+#>
+
+[System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSReviewUnusedParameter', '', Justification = 'Parameter is used actually.')]
+param(
+    # Build Version
+    [Parameter()]
+    [string]
+    $Version,
+    # Build Instance
+    [Parameter()]
+    [string]
+    $Instance
+)
+
+Set-StrictMode -Version Latest
+
+
+# Synopsis: Restore
+Task Restore RestoreWorkloads, RestoreTools, RestorePackages
+
+# Synopsis: Restore workloads
+Task RestoreWorkloads Clean, {
+    Exec { dotnet workload restore }
+}
+
+# Synopsis: Restore tools
+Task RestoreTools Clean, {
+    Exec { dotnet tool restore }
+}
+
+# Synopsis: Restore packages
+Task RestorePackages Clean, {
+    $solution = Resolve-Path -Path 'API.sln'
+    Exec { dotnet restore $solution }
+}
+
+# Synopsis: Clean previous build leftovers
+Task Clean Init, {
+    Get-ChildItem -Directory
+    | Where-Object { -not $_.Name.StartsWith('.') }
+    | ForEach-Object { Get-ChildItem -Path $_ -Recurse -Directory }
+    | Where-Object { ( $_.Name -eq 'bin') -or ( $_.Name -eq 'obj') }
+    | ForEach-Object { Remove-Item -Path $_ -Recurse -Force }
+}
+
+# Synopsis: Initialize folders and variables
+Task Init {
+    $trashFolder = Join-Path -Path . -ChildPath '.trash'
+    $trashFolder = Join-Path -Path $trashFolder -ChildPath $Instance
+    New-Item -Path $trashFolder -ItemType Directory | Out-Null
+    $trashFolder = Resolve-Path -Path $trashFolder
+
+    $buildArtifactsFolder = Join-Path -Path $trashFolder -ChildPath 'artifacts'
+    New-Item -Path $buildArtifactsFolder -ItemType Directory | Out-Null
+
+    $anyBuildArtifactsFolder = Join-Path -Path $buildArtifactsFolder -ChildPath 'any'
+    New-Item -Path $anyBuildArtifactsFolder -ItemType Directory | Out-Null
+
+    $state = [PSCustomObject]@{
+        NextVersion                        = $null
+        TrashFolder                        = $trashFolder
+        BuildArtifactsFolder               = $buildArtifactsFolder
+        AnyBuildArtifactsFolder            = $anyBuildArtifactsFolder
+    }
+
+    $state | Export-Clixml -Path ".\.trash\$Instance\state.clixml"
+    Write-Output $state
+}
