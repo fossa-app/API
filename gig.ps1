a36bc0efb9a5b343c16c0e7a126fd07729f41389
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-06-22 17:03:35 +0200

    Update gig.ps1

--- gig.ps1
+++ gig.ps1
@@ -5,17 +5,14 @@ Function gig {
         [string[]]$list
     )
     $params = ($list | ForEach-Object { [uri]::EscapeDataString($_) }) -join ','
-    Invoke-WebRequest -Uri "https://www.toptal.com/developers/gitignore/api/$params" | Select-Object -ExpandProperty content | Out-File -FilePath $(Join-Path -Path $pwd -ChildPath '.gitignore') -Encoding ascii
+    Invoke-WebRequest -Uri "https://www.toptal.com/developers/gitignore/api/$params"
+    | Select-Object -ExpandProperty content | Out-File -FilePath $(Join-Path -Path $pwd -ChildPath '.gitignore') -Encoding ascii
 }
 
-
-gig -list visualstudio, visualstudiocode, rider, powershell, fsharp
+gig -list visualstudio, visualstudiocode, rider, fsharp, powershell, macos, intellij+all
 
 Add-Content -Path .\.gitignore -Value '# Repository Specific' -Encoding ascii
+Add-Content -Path .\.gitignore -Value '.trash' -Encoding ascii
 Add-Content -Path .\.gitignore -Value '*.received.*' -Encoding ascii
 Add-Content -Path .\.gitignore -Value '*.received/' -Encoding ascii
 Add-Content -Path .\.gitignore -Value '.idea/' -Encoding ascii
-Add-Content -Path .\.gitignore -Value 'src/API.Web/database.sqlite' -Encoding ascii
-Add-Content -Path .\.gitignore -Value 'src/API.Web/database.sqlite-shm' -Encoding ascii
-Add-Content -Path .\.gitignore -Value 'src/API.Web/database.sqlite-wal' -Encoding ascii
-Add-Content -Path .\.gitignore -Value 'src/API.Web/log*.txt' -Encoding ascii
