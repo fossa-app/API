37b0be62b1f32e0340f7c7e017188a9bf3d2a5d5
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-08-28 17:17:02 -0500

    Implement ActiveFusionAuthApplicationFilter

---
+++ src/API.Infrastructure/Messages/Queries/FusionAuthApplicationFilters/ActiveFusionAuthApplicationFilter.cs
@@ -0,0 +1,15 @@
+﻿using Fossa.API.Infrastructure.Models;
+using LanguageExt;
+
+namespace Fossa.API.Infrastructure.Messages.Queries.FusionAuthApplicationFilters;
+
+public class ActiveFusionAuthApplicationFilter : IFusionAuthApplicationFilter
+{
+  public Task<Seq<FusionAuthApplication>> FilterAsync(
+    Seq<FusionAuthApplication> fusionAuthApplications,
+    IdentityClientRetrievalQuery query,
+    CancellationToken cancellationToken)
+  {
+    return Task.FromResult(fusionAuthApplications.Filter(x => x.Active));
+  }
+}
---
+++ src/API.Infrastructure/Messages/Queries/FusionAuthApplicationFilters/IFusionAuthApplicationFilter.cs
@@ -0,0 +1,12 @@
+﻿using Fossa.API.Infrastructure.Models;
+using LanguageExt;
+
+namespace Fossa.API.Infrastructure.Messages.Queries.FusionAuthApplicationFilters;
+
+public interface IFusionAuthApplicationFilter
+{
+  Task<Seq<FusionAuthApplication>> FilterAsync(
+    Seq<FusionAuthApplication> fusionAuthApplications,
+    IdentityClientRetrievalQuery query,
+    CancellationToken cancellationToken);
+}
