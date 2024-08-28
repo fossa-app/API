cab19f2c22dc29cd9504ba6ce84078f2a467487e
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-08-27 19:42:16 -0500

    Create Fusion Auth Application Models

---
+++ src/API.Infrastructure/Models/FusionAuthApplicationOauthConfiguration.cs
@@ -0,0 +1,60 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Fossa.API.Infrastructure.Models;
+
+public partial class FusionAuthApplicationOauthConfiguration
+{
+  [JsonPropertyName("authorizedOriginURLs")]
+  public string[]? AuthorizedOriginUrLs { get; set; }
+
+  [JsonPropertyName("authorizedRedirectURLs")]
+  public string[]? AuthorizedRedirectUrLs { get; set; }
+
+  [JsonPropertyName("authorizedURLValidationPolicy")]
+  public string? AuthorizedUrlValidationPolicy { get; set; }
+
+  [JsonPropertyName("clientAuthenticationPolicy")]
+  public string? ClientAuthenticationPolicy { get; set; }
+
+  [JsonPropertyName("clientId")]
+  public Guid ClientId { get; set; }
+
+  [JsonPropertyName("clientSecret")]
+  public string? ClientSecret { get; set; }
+
+  [JsonPropertyName("consentMode")]
+  public string? ConsentMode { get; set; }
+
+  [JsonPropertyName("debug")]
+  public bool Debug { get; set; }
+
+  [JsonPropertyName("enabledGrants")]
+  public string[]? EnabledGrants { get; set; }
+
+  [JsonPropertyName("generateRefreshTokens")]
+  public bool GenerateRefreshTokens { get; set; }
+
+  [JsonPropertyName("logoutBehavior")]
+  public string? LogoutBehavior { get; set; }
+
+  [JsonPropertyName("proofKeyForCodeExchangePolicy")]
+  public string? ProofKeyForCodeExchangePolicy { get; set; }
+
+  [JsonPropertyName("relationship")]
+  public string? Relationship { get; set; }
+
+  [JsonPropertyName("requireClientAuthentication")]
+  public bool RequireClientAuthentication { get; set; }
+
+  [JsonPropertyName("requireRegistration")]
+  public bool RequireRegistration { get; set; }
+
+  [JsonPropertyName("scopeHandlingPolicy")]
+  public string? ScopeHandlingPolicy { get; set; }
+
+  [JsonPropertyName("unknownScopePolicy")]
+  public string? UnknownScopePolicy { get; set; }
+
+  [JsonPropertyName("logoutURL")]
+  public string? LogoutUrl { get; set; }
+}
---
+++ src/API.Infrastructure/Models/FusionAuthApplicationsListingResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Fossa.API.Infrastructure.Models;
+
+public partial class FusionAuthApplicationsListingResponse
+{
+  [JsonPropertyName("applications")]
+  public FusionAuthApplication[]? Applications { get; set; }
+}
