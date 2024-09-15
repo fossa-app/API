a1213df5f86429b5fb73276d54005b72dc88df70
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-09-15 17:31:39 -0500

    Create BranchMongoRepository.cs

---
+++ src/API.Persistence/Mongo/Repositories/BranchMongoRepository.cs
@@ -0,0 +1,33 @@
+﻿using Fossa.API.Core.Repositories;
+using Fossa.API.Persistence.Mongo.Entities;
+using MongoDB.Driver;
+using TIKSN.Data;
+using TIKSN.Data.Mongo;
+
+namespace Fossa.API.Persistence.Mongo.Repositories;
+
+public class BranchMongoRepository
+  : MongoRepository<BranchMongoEntity, long>, IBranchMongoRepository
+{
+  public BranchMongoRepository(
+    IMongoClientSessionProvider mongoClientSessionProvider,
+    IMongoDatabaseProvider mongoDatabaseProvider) : base(
+    mongoClientSessionProvider,
+    mongoDatabaseProvider,
+    "Branches")
+  {
+  }
+
+  protected override SortDefinition<BranchMongoEntity> PageSortDefinition
+    => Builders<BranchMongoEntity>.Sort.Ascending(x => x.ID);
+
+  public Task<PageResult<BranchMongoEntity>> PageAsync(
+    TenantBranchPageQuery pageQuery,
+    CancellationToken cancellationToken)
+  {
+    var filter =
+      Builders<BranchMongoEntity>.Filter.Eq(item => item.TenantID, pageQuery.TenantId);
+
+    return this.PageAsync(filter, pageQuery, cancellationToken);
+  }
+}
