628ee956a49fd17263c7cde186bfd6f9a3ff0b09
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-09-15 18:37:03 -0500

    Create BranchRetrievalQueryHandler.cs

---
+++ src/API.Core/Messages/Queries/BranchRetrievalQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using Fossa.API.Core.Entities;
+using Fossa.API.Core.Repositories;
+using MediatR;
+
+namespace Fossa.API.Core.Messages.Queries;
+
+public class BranchRetrievalQueryHandler : IRequestHandler<BranchRetrievalQuery, BranchEntity>
+{
+  private readonly IBranchQueryRepository _branchQueryRepository;
+
+  public BranchRetrievalQueryHandler(
+    IBranchQueryRepository branchQueryRepository)
+  {
+    _branchQueryRepository = branchQueryRepository ?? throw new ArgumentNullException(nameof(branchQueryRepository));
+  }
+
+  public async Task<BranchEntity> Handle(
+    BranchRetrievalQuery request,
+    CancellationToken cancellationToken)
+  {
+    return await _branchQueryRepository.GetAsync(
+        request.ID, cancellationToken)
+      .ConfigureAwait(false);
+  }
+}
