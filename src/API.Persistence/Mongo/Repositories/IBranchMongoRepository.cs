1fd4d6e4d7a451c55a7cad0c91cbfb7f6460153c
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-09-15 17:30:56 -0500

    Create IBranchMongoRepository.cs

---
+++ src/API.Persistence/Mongo/Repositories/IBranchMongoRepository.cs
@@ -0,0 +1,13 @@
+﻿using Fossa.API.Core.Repositories;
+using Fossa.API.Persistence.Mongo.Entities;
+using TIKSN.Data;
+using TIKSN.Data.Mongo;
+
+namespace Fossa.API.Persistence.Mongo.Repositories;
+
+public interface IBranchMongoRepository : IMongoRepository<BranchMongoEntity, long>
+{
+  Task<PageResult<BranchMongoEntity>> PageAsync(
+    TenantBranchPageQuery pageQuery,
+    CancellationToken cancellationToken);
+}
