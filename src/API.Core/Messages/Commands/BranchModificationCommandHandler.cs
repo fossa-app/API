363c52defd79bec806e6483543a87254f801bac6
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-09-15 18:38:39 -0500

    Create BranchModificationCommandHandler.cs

---
+++ src/API.Core/Messages/Commands/BranchModificationCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using Fossa.API.Core.Repositories;
+using MediatR;
+
+namespace Fossa.API.Core.Messages.Commands;
+
+public class BranchModificationCommandHandler : IRequestHandler<BranchModificationCommand, Unit>
+{
+  private readonly IBranchQueryRepository _branchQueryRepository;
+  private readonly IBranchRepository _branchRepository;
+
+  public BranchModificationCommandHandler(
+    IBranchQueryRepository branchQueryRepository,
+    IBranchRepository branchRepository)
+  {
+    _branchQueryRepository = branchQueryRepository ?? throw new ArgumentNullException(nameof(branchQueryRepository));
+    _branchRepository = branchRepository ?? throw new ArgumentNullException(nameof(branchRepository));
+  }
+
+  public async Task<Unit> Handle(
+    BranchModificationCommand request,
+    CancellationToken cancellationToken)
+  {
+    var entity = await _branchQueryRepository.GetAsync(request.ID, cancellationToken).ConfigureAwait(false);
+    entity = entity with
+    {
+      Name = request.Name,
+    };
+    await _branchRepository.UpdateAsync(entity, cancellationToken).ConfigureAwait(false);
+    return Unit.Value;
+  }
+}
