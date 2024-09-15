861e1f0c78fa1a716dc6842fbf310cc1a2ccd76c
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-09-15 18:37:39 -0500

    Create BranchPagingQuery.cs

---
+++ src/API.Core/Messages/Queries/BranchPagingQuery.cs
@@ -0,0 +1,15 @@
+﻿using Fossa.API.Core.Entities;
+using TIKSN.Data;
+
+namespace Fossa.API.Core.Messages.Queries;
+
+public record BranchPagingQuery(
+    Guid TenantID,
+    Guid UserID,
+    Page Page)
+  : EntityTenantQuery<BranchEntity, BranchId, Guid, PageResult<BranchEntity>>(TenantID)
+    , IPagingQuery<BranchEntity>
+{
+  public override IEnumerable<BranchId> AffectingTenantEntitiesIdentities
+    => Enumerable.Empty<BranchId>();
+}
