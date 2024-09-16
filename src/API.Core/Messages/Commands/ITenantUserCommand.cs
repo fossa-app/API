b6cb5a1e4e7846abc29b71d77750c53600f536bf
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-09-15 21:28:00 -0500

    Update ITenantUserCommand.cs

--- src/API.Core/Messages/Commands/ITenantUserCommand.cs
+++ src/API.Core/Messages/Commands/ITenantUserCommand.cs
@@ -1,8 +1,8 @@
 ﻿namespace Fossa.API.Core.Messages.Commands;
 
-public interface ITenantUserCommand<TEntityIdentity, out TUserIdentity, out TTenantIdentity>
+public interface ITenantUserCommand<TEntityIdentity, out TTenantIdentity, out TUserIdentity>
   : ITenantCommand<TEntityIdentity, TTenantIdentity>
   , IUserCommand<TUserIdentity>
   where TEntityIdentity : IEquatable<TEntityIdentity>
-  where TUserIdentity : IEquatable<TUserIdentity>
-  where TTenantIdentity : IEquatable<TTenantIdentity>;
+  where TTenantIdentity : IEquatable<TTenantIdentity>
+  where TUserIdentity : IEquatable<TUserIdentity>;
