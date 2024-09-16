27fd833c057e2329c7d68f1bc4d13372af65759b
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-09-15 20:17:27 -0500

    Create BranchRetrievalQueryValidator.cs

---
+++ src/API.Core/Validators/BranchRetrievalQueryValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Fossa.API.Core.Messages.Queries;
+
+namespace Fossa.API.Core.Validators;
+
+public class BranchRetrievalQueryValidator : AbstractValidator<BranchRetrievalQuery>
+{
+  public BranchRetrievalQueryValidator()
+  {
+    RuleFor(x => x.ID).NotEmpty();
+    RuleFor(x => x.TenantID).NotEmpty();
+    RuleFor(x => x.UserID).NotEmpty();
+  }
+}
