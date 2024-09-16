a732c59577576f87f61d455bb9038583226ccd02
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-09-15 21:28:04 -0500

    Create EntityTenantUserCommand.cs

---
+++ src/API.Core/Messages/Commands/EntityTenantUserCommand.cs
@@ -0,0 +1,18 @@
+﻿using TIKSN.Data;
+
+namespace Fossa.API.Core.Messages.Commands;
+
+public abstract record EntityTenantUserCommand<TEntity, TEntityIdentity, TTenantIdentity, TUserIdentity>(
+  TTenantIdentity TenantID,
+  TUserIdentity UserID)
+  : ITenantUserCommand<TEntityIdentity, TTenantIdentity, TUserIdentity>
+  where TEntity : IEntity<TEntityIdentity>
+  where TEntityIdentity : IEquatable<TEntityIdentity>
+  where TTenantIdentity : IEquatable<TTenantIdentity>
+  where TUserIdentity : IEquatable<TUserIdentity>
+{
+  public abstract IEnumerable<TEntityIdentity> AffectingTenantEntitiesIdentities { get; }
+
+  public IEnumerable<AffectingEntity<TEntityIdentity>> AffectingTenantEntities
+    => AffectingTenantEntitiesIdentities.Select(x => new AffectingEntity<TEntityIdentity>(typeof(TEntity), x));
+}
