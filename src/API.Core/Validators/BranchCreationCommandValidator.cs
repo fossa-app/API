6adc5b9e9632fd7d5a91df37f8b5735910d230de
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-09-15 20:17:10 -0500

    Create BranchCreationCommandValidator.cs

---
+++ src/API.Core/Validators/BranchCreationCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Fossa.API.Core.Messages.Commands;
+
+namespace Fossa.API.Core.Validators;
+
+public class BranchCreationCommandValidator : AbstractValidator<BranchCreationCommand>
+{
+  public BranchCreationCommandValidator()
+  {
+    RuleFor(x => x.TenantID).NotEmpty();
+    RuleFor(x => x.UserID).NotEmpty();
+    RuleFor(x => x.Name).NotEmpty();
+  }
+}
