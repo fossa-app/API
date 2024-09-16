4398db086f2f7b1604e5d82096f65701bf999cf2
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-09-15 20:17:23 -0500

    Create BranchModificationCommandValidator.cs

---
+++ src/API.Core/Validators/BranchModificationCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using Fossa.API.Core.Messages.Commands;
+
+namespace Fossa.API.Core.Validators;
+
+public class BranchModificationCommandValidator : AbstractValidator<BranchModificationCommand>
+{
+  public BranchModificationCommandValidator()
+  {
+    RuleFor(x => x.ID).NotEmpty();
+    RuleFor(x => x.TenantID).NotEmpty();
+    RuleFor(x => x.UserID).NotEmpty();
+    RuleFor(x => x.Name).NotEmpty();
+  }
+}
