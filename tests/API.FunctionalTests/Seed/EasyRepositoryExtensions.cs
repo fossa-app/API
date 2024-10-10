cbbefa3041c9841b85c2e810f15233d6e2d8149a
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-10-09 19:36:34 -0500

    Implement Company Retrieval Tests

---
+++ tests/API.FunctionalTests/Seed/EasyRepositoryExtensions.cs
@@ -0,0 +1,20 @@
+﻿using EasyDoubles;
+using TIKSN.Data;
+
+namespace Fossa.API.FunctionalTests.Seed;
+
+public static class EasyRepositoryExtensions
+{
+  public static async Task TryAddAsync<TEntity, TIdentity>(
+    this IEasyRepository<TEntity, TIdentity> easyRepository,
+    TEntity entity,
+    CancellationToken cancellationToken)
+    where TEntity : IEntity<TIdentity>
+    where TIdentity : IEquatable<TIdentity>
+  {
+    if (!await easyRepository.ExistsAsync(entity.ID, cancellationToken).ConfigureAwait(false))
+    {
+      await easyRepository.AddAsync(entity, cancellationToken).ConfigureAwait(false);
+    }
+  }
+}
