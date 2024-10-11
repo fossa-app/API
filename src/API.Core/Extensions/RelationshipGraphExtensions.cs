994a6baec793015847fda4e65feb8167f5843846
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-10-11 15:57:56 -0500

    Create RelationshipGraphExtensions.cs

---
+++ src/API.Core/Extensions/RelationshipGraphExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Fossa.API.Core.Messages;
+using Fossa.API.Core.Relationship;
+
+namespace Fossa.API.Core.Extensions;
+
+public static class RelationshipGraphExtensions
+{
+  public static async Task ThrowIfHasDependentEntitiesAsync<T>(this IRelationshipGraph relationshipGraph, T id, CancellationToken cancellationToken) where T : IEquatable<T>
+  {
+    ArgumentNullException.ThrowIfNull(relationshipGraph);
+
+    var hasDependentEntities = await relationshipGraph.HasDependentEntitiesAsync(id, cancellationToken).ConfigureAwait(false);
+
+    if (hasDependentEntities)
+    {
+      throw new FailedDependencyException($"Entity with {id.GetType().Name}({id}) has dependent entities.");
+    }
+  }
+}
