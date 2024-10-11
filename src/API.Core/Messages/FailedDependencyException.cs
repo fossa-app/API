82e4090273ff25b6603229e6207de6d3989177ca
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-10-11 15:57:39 -0500

    Create FailedDependencyException.cs

---
+++ src/API.Core/Messages/FailedDependencyException.cs
@@ -0,0 +1,8 @@
+﻿namespace Fossa.API.Core.Messages;
+
+public class FailedDependencyException : Exception
+{
+  public FailedDependencyException() { }
+  public FailedDependencyException(string message) : base(message) { }
+  public FailedDependencyException(string message, Exception inner) : base(message, inner) { }
+}
