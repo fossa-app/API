1e3ef8ffdfc14b5d3cba362372575475849b57b2
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-10-12 00:22:42 -0500

    Create EmployeeDeletionCommandHandler.cs

---
+++ src/API.Core/Messages/Commands/EmployeeDeletionCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using Fossa.API.Core.Entities;
+using Fossa.API.Core.Extensions;
+using Fossa.API.Core.Relationship;
+using Fossa.API.Core.Repositories;
+using MediatR;
+
+namespace Fossa.API.Core.Messages.Commands;
+
+public class EmployeeDeletionCommandHandler : IRequestHandler<EmployeeDeletionCommand, Unit>
+{
+  private readonly IEmployeeQueryRepository _employeeQueryRepository;
+  private readonly IEmployeeRepository _employeeRepository;
+  private readonly IRelationshipGraph _relationshipGraph;
+
+  public EmployeeDeletionCommandHandler(
+    IEmployeeQueryRepository employeeQueryRepository,
+    IEmployeeRepository employeeRepository,
+    IRelationshipGraph relationshipGraph)
+  {
+    _employeeQueryRepository = employeeQueryRepository ?? throw new ArgumentNullException(nameof(employeeQueryRepository));
+    _employeeRepository = employeeRepository ?? throw new ArgumentNullException(nameof(employeeRepository));
+    _relationshipGraph = relationshipGraph ?? throw new ArgumentNullException(nameof(relationshipGraph));
+  }
+
+  public async Task DeleteEmployeeAsync(
+    EmployeeEntity entity,
+    CancellationToken cancellationToken)
+  {
+    await _relationshipGraph.ThrowIfHasDependentEntitiesAsync(entity.ID, cancellationToken).ConfigureAwait(false);
+    await _employeeRepository.RemoveAsync(entity, cancellationToken).ConfigureAwait(false);
+  }
+
+  public async Task<Unit> Handle(
+    EmployeeDeletionCommand request,
+    CancellationToken cancellationToken)
+  {
+    var entityMaybe = await _employeeQueryRepository.FindByUserIdAsync(request.UserID, cancellationToken).ConfigureAwait(false);
+    await entityMaybe.IfSomeAsync(
+      entity => DeleteEmployeeAsync(entity, cancellationToken)).ConfigureAwait(false);
+    return Unit.Value;
+  }
+}
