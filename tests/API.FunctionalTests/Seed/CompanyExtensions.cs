8412d55b1364d1a1a958b033d08ea5138a10c21f
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-10-11 23:45:50 -0500

    Update CompanyExtensions.cs

--- tests/API.FunctionalTests/Seed/CompanyExtensions.cs
+++ tests/API.FunctionalTests/Seed/CompanyExtensions.cs
@@ -20,5 +20,12 @@ public static class CompanyExtensions
       TenantID = Guid.Parse("53ade3c2-8e36-52f2-88cf-d068b1ab247a"),
       Name = "Company1",
     }, cancellationToken).ConfigureAwait(false);
+
+    await companyRepository.TryAddAsync(new CompanyMongoEntity
+    {
+      ID = 200L,
+      TenantID = Guid.Parse("cf59d3dd-5258-5a20-88ab-0169cf128440"),
+      Name = "Company2",
+    }, cancellationToken).ConfigureAwait(false);
   }
 }
