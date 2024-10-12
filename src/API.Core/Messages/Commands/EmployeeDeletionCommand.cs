e6a76329a0669cf7c8580c09d70aec49f6ea2907
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-10-11 23:54:59 -0500

    Create EmployeeDeletionCommand.cs

---
+++ src/API.Core/Messages/Commands/EmployeeDeletionCommand.cs
@@ -0,0 +1,12 @@
+﻿using Fossa.API.Core.Entities;
+
+namespace Fossa.API.Core.Messages.Commands;
+
+public record EmployeeDeletionCommand(
+    Guid TenantID,
+    Guid UserID)
+  : EntityTenantUserCommand<EmployeeEntity, EmployeeId, Guid, Guid>(TenantID, UserID)
+{
+  public override IEnumerable<EmployeeId> AffectingTenantEntitiesIdentities
+    => [];
+}
