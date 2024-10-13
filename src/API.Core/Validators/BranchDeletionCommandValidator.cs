da5b25d44865fa3c813d840711262f2d75d6773d
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-10-12 19:11:05 -0500

    Create BranchDeletionCommandValidator.cs

---
+++ src/API.Core/Validators/BranchDeletionCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Fossa.API.Core.Messages.Commands;
+
+namespace Fossa.API.Core.Validators;
+
+public class BranchDeletionCommandValidator : AbstractValidator<BranchDeletionCommand>
+{
+  public BranchDeletionCommandValidator()
+  {
+    RuleFor(x => x.ID).NotEmpty();
+    RuleFor(x => x.TenantID).NotEmpty();
+    RuleFor(x => x.UserID).NotEmpty();
+  }
+}
