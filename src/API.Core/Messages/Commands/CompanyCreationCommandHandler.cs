fb0f6ba1e89786e1a5877ee4c8a623d7932b489f
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-10-12 23:12:15 -0500

    Initialize company license after company creation

--- src/API.Core/Messages/Commands/CompanyCreationCommandHandler.cs
+++ src/API.Core/Messages/Commands/CompanyCreationCommandHandler.cs
@@ -11,6 +11,7 @@ namespace Fossa.API.Core.Messages.Commands;
 
 public class CompanyCreationCommandHandler : IRequestHandler<CompanyCreationCommand, Unit>
 {
+  private readonly ICompanyLicenseInitializer _companyLicenseInitializer;
   private readonly ICompanyQueryRepository _companyQueryRepository;
   private readonly ICompanyRepository _companyRepository;
   private readonly IIdentityGenerator<CompanyId> _identityGenerator;
@@ -19,11 +20,13 @@ public class CompanyCreationCommandHandler : IRequestHandler<CompanyCreationComm
   public CompanyCreationCommandHandler(
     IIdentityGenerator<CompanyId> identityGenerator,
     ISystemLicenseRetriever systemLicenseRetriever,
+    ICompanyLicenseInitializer companyLicenseInitializer,
     ICompanyQueryRepository companyQueryRepository,
     ICompanyRepository companyRepository)
   {
     _identityGenerator = identityGenerator ?? throw new ArgumentNullException(nameof(identityGenerator));
     _systemLicenseRetriever = systemLicenseRetriever ?? throw new ArgumentNullException(nameof(systemLicenseRetriever));
+    _companyLicenseInitializer = companyLicenseInitializer ?? throw new ArgumentNullException(nameof(companyLicenseInitializer));
     _companyQueryRepository = companyQueryRepository ?? throw new ArgumentNullException(nameof(companyQueryRepository));
     _companyRepository = companyRepository ?? throw new ArgumentNullException(nameof(companyRepository));
   }
@@ -43,8 +46,15 @@ public class CompanyCreationCommandHandler : IRequestHandler<CompanyCreationComm
     var id = _identityGenerator.Generate();
     CompanyEntity entity = new(id, request.TenantID, request.Name);
     await _companyRepository.AddAsync(entity, cancellationToken).ConfigureAwait(false);
+    await _companyLicenseInitializer.InitializeAsync(id, cancellationToken).ConfigureAwait(false);
 
     return Unit.Value;
+  }
+
+  private static bool EnsureMaximumCompanyCountWillNotExceed(
+    License<SystemEntitlements> license, int numberOfCompanies)
+  {
+    return license.Entitlements.MaximumCompanyCount > numberOfCompanies;
   }
 
   private async Task ValidateEntitlementsAsync(CancellationToken cancellationToken)
@@ -60,11 +70,5 @@ public class CompanyCreationCommandHandler : IRequestHandler<CompanyCreationComm
         "The current system license entitlements limit the number of companies that can be created, and this limit has been reached");
 
     _ = licenseValidation.GetOrThrow();
-  }
-
-  private static bool EnsureMaximumCompanyCountWillNotExceed(
-    License<SystemEntitlements> license, int numberOfCompanies)
-  {
-    return license.Entitlements.MaximumCompanyCount > numberOfCompanies;
   }
 }
