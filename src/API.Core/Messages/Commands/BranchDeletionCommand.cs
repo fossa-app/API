f9c1b750a6c2448095705da750dc65c2e13c5545
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-10-12 19:10:57 -0500

    Create BranchDeletionCommand.cs

---
+++ src/API.Core/Messages/Commands/BranchDeletionCommand.cs
@@ -0,0 +1,14 @@
+﻿using Fossa.API.Core.Entities;
+using LanguageExt;
+
+namespace Fossa.API.Core.Messages.Commands;
+
+public record BranchDeletionCommand(
+  BranchId ID,
+  Guid TenantID,
+  Guid UserID)
+: EntityTenantCommand<BranchEntity, BranchId, Guid>(TenantID)
+{
+  public override IEnumerable<BranchId> AffectingTenantEntitiesIdentities
+    => Prelude.Seq1(ID);
+}
