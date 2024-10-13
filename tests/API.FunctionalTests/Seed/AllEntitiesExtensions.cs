1786aedaf61bb709120ac63d1f58028832a16d91
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-10-12 19:11:14 -0500

    Create AllEntitiesExtensions.cs

---
+++ tests/API.FunctionalTests/Seed/AllEntitiesExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Fossa.API.FunctionalTests.Seed;
+
+public static class AllEntitiesExtensions
+{
+  public static async Task SeedAllEntitiesAsync<TEntryPoint>(
+    this WebApplicationFactory<TEntryPoint> factory,
+    CancellationToken cancellationToken)
+    where TEntryPoint : class
+  {
+    await factory.SeedCompaniesAsync(default).ConfigureAwait(false);
+    await factory.SeedBranchesAsync(default).ConfigureAwait(false);
+    await factory.SeedEmployeesAsync(default).ConfigureAwait(false);
+  }
+}
+
