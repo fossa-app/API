4a0dc24170cf0d5b600b1c6e64773ce05d39843e
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-10-13 00:23:02 -0500

    Handle CompanyLicenseRetrievalQuery

---
+++ src/API.Core/Messages/Queries/CompanyLicenseRetrievalQuery.cs
@@ -0,0 +1,6 @@
+﻿using Fossa.Licensing;
+using TIKSN.Licensing;
+
+namespace Fossa.API.Core.Messages.Queries;
+
+public record CompanyLicenseRetrievalQuery(Guid TenantID) : IQuery<License<CompanyEntitlements>>;
