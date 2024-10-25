ac8edb91366097a3bca5e223c976c0ac8ef0d82e
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-10-24 21:57:59 -0500

    Implement Employee Creation Functional Tests

--- tests/API.FunctionalTests/ControllerApis/EmployeeControllerWithSystemLicense.cs
+++ tests/API.FunctionalTests/ControllerApis/EmployeeControllerWithSystemLicense.cs
@@ -21,6 +21,72 @@ public class EmployeeControllerWithSystemLicense : IClassFixture<CustomWebApplic
   }
 
   [Fact]
+  public async Task CreateEmployeeWithoutAccessTokenAsync()
+  {
+    var client = _factory.CreateClient();
+
+    const string firstName = "First576536102";
+    const string lastName = "Last576536102";
+    const string fullName = "Full576536102";
+
+    var response = await client.PostAsJsonAsync("/api/1.0/Employee", new EmployeeModificationModel(firstName, lastName, fullName));
+
+    response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+  }
+
+  [Fact]
+  public async Task CreateExistingEmployeeWithAccessTokenAsync()
+  {
+    var client = _factory.CreateClient();
+    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "01JA1ZJAWF27S0J8Z2VJE7673Y.Tenant1.User1");
+
+    const string firstName = "First812685875";
+    const string lastName = "Last812685875";
+    const string fullName = "Full812685875";
+
+    var creationResponse = await client.PostAsJsonAsync("/api/1.0/Employee", new EmployeeModificationModel(firstName, lastName, fullName));
+
+    creationResponse.StatusCode.ShouldBe(HttpStatusCode.Conflict);
+
+    var retrievalResponse = await client.GetAsync("/api/1.0/Employee");
+
+    var responseModel =
+      await retrievalResponse.Content.ReadFromJsonAsync<EmployeeRetrievalModel>();
+
+    responseModel.ShouldNotBeNull();
+    responseModel.Id.ShouldBePositive();
+    responseModel.FirstName.ShouldNotBe(firstName);
+    responseModel.LastName.ShouldNotBe(lastName);
+    responseModel.FullName.ShouldNotBe(fullName);
+  }
+
+  [Fact]
+  public async Task CreateMissingEmployeeWithAccessTokenAsync()
+  {
+    var client = _factory.CreateClient();
+    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "01JA1ZJAWF27S0J8Z2VJE7673Y.Tenant1.User100");
+
+    const string firstName = "First576536102";
+    const string lastName = "Last576536102";
+    const string fullName = "Full576536102";
+
+    var creationResponse = await client.PostAsJsonAsync("/api/1.0/Employee", new EmployeeModificationModel(firstName, lastName, fullName));
+
+    creationResponse.StatusCode.ShouldBe(HttpStatusCode.OK);
+
+    var retrievalResponse = await client.GetAsync("/api/1.0/Employee");
+
+    var responseModel =
+      await retrievalResponse.Content.ReadFromJsonAsync<EmployeeRetrievalModel>();
+
+    responseModel.ShouldNotBeNull();
+    responseModel.Id.ShouldBePositive();
+    responseModel.FirstName.ShouldBe(firstName);
+    responseModel.LastName.ShouldBe(lastName);
+    responseModel.FullName.ShouldBe(fullName);
+  }
+
+  [Fact]
   public async Task DeleteEmployeeWithoutAccessTokenAsync()
   {
     var client = _factory.CreateClient();
@@ -77,7 +143,8 @@ public class EmployeeControllerWithSystemLicense : IClassFixture<CustomWebApplic
     var client = _factory.CreateClient();
     client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "01J9ZHGBMC1DVP4R33QRYZ04RX.Tenant1.User1");
     var response = await client.GetAsync("/api/1.0/Employee");
-    response.EnsureSuccessStatusCode();
+
+    response.StatusCode.ShouldBe(HttpStatusCode.OK);
 
     var responseModel =
       await response.Content.ReadFromJsonAsync<EmployeeRetrievalModel>();
