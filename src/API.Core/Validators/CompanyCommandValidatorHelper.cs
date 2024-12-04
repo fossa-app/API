b9731ca80b6e124d928cea1c3b7bd5ab292568a7
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-12-03 18:01:17 -0600

    Add Company Creation and Modification validators for Country

---
+++ src/API.Core/Validators/CompanyCommandValidatorHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using Fossa.API.Core.Services;
+
+namespace Fossa.API.Core.Validators;
+
+public static class CompanyCommandValidatorHelper
+{
+  public static async Task<bool> CompanyCountryMustBeLicensedAsync(
+    ISystemLicenseRetriever systemLicenseRetriever,
+    RegionInfo companyCountry,
+    CancellationToken cancellationToken)
+  {
+    var systemLicenseResult = await systemLicenseRetriever.GetAsync(cancellationToken).ConfigureAwait(false);
+
+    return systemLicenseResult.Match(license => license.Entitlements.Countries.Contains(companyCountry), _ => false);
+  }
+}
