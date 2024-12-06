1e57013c9764a1505fa03a0387582a710fd99c18
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-12-05 23:55:35 -0600

    Replace lookups

--- src/API.Web/Mappers/SystemEntitlementsModelMapper.cs
+++ src/API.Web/Mappers/SystemEntitlementsModelMapper.cs
@@ -1,9 +1,9 @@
 ﻿using System.Globalization;
+using Fossa.API.Core.Services;
 using Fossa.API.Web.ApiModels;
 using Fossa.Licensing;
 using LanguageExt;
 using NodaTime;
-using NodaTime.TimeZones;
 using TIKSN.Mapping;
 
 namespace Fossa.API.Web.Mappers;
@@ -11,15 +11,18 @@ namespace Fossa.API.Web.Mappers;
 public class SystemEntitlementsModelMapper : IMapper<SystemEntitlements, SystemEntitlementsModel>
 {
   private readonly IMapper<RegionInfo, CountryModel> _countryModelMapper;
+  private readonly IDateTimeZoneLookup _dateTimeZoneLookup;
   private readonly IHostEnvironment _hostEnvironment;
   private readonly IMapper<DateTimeZone, TimeZoneModel> _timeZoneModelMapper;
 
   public SystemEntitlementsModelMapper(
     IHostEnvironment hostEnvironment,
+    IDateTimeZoneLookup dateTimeZoneLookup,
     IMapper<RegionInfo, CountryModel> countryModelMapper,
     IMapper<DateTimeZone, TimeZoneModel> timeZoneModelMapper)
   {
     _hostEnvironment = hostEnvironment ?? throw new ArgumentNullException(nameof(hostEnvironment));
+    _dateTimeZoneLookup = dateTimeZoneLookup ?? throw new ArgumentNullException(nameof(dateTimeZoneLookup));
     _countryModelMapper = countryModelMapper ?? throw new ArgumentNullException(nameof(countryModelMapper));
     _timeZoneModelMapper = timeZoneModelMapper ?? throw new ArgumentNullException(nameof(timeZoneModelMapper));
   }
@@ -27,8 +30,7 @@ public class SystemEntitlementsModelMapper : IMapper<SystemEntitlements, SystemE
   public SystemEntitlementsModel Map(SystemEntitlements source)
   {
     var timeZones = source.Countries
-      .SelectMany(x => TzdbDateTimeZoneSource.Default.ZoneLocations?.Where(z => string.Equals(x.TwoLetterISORegionName, z.CountryCode, StringComparison.OrdinalIgnoreCase)) ?? [])
-      .Select(x => DateTimeZoneProviders.Tzdb[x.ZoneId])
+      .SelectMany(x => _dateTimeZoneLookup.ListRegionalTimeZones(x))
       .ToSeq();
 
     return new SystemEntitlementsModel(
