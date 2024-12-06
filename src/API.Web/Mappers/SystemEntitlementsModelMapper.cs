f7c3bc6d55598ba741562c1d87ee181a26cffc27
Tigran TIKSN Torosyan <personal@tiksn.com>
2024-12-05 23:22:38 -0600

    Return TimeZone Current Offset

--- src/API.Web/Mappers/SystemEntitlementsModelMapper.cs
+++ src/API.Web/Mappers/SystemEntitlementsModelMapper.cs
@@ -1,6 +1,7 @@
 ﻿using System.Globalization;
 using Fossa.API.Web.ApiModels;
 using Fossa.Licensing;
+using LanguageExt;
 using NodaTime;
 using NodaTime.TimeZones;
 using TIKSN.Mapping;
@@ -11,17 +12,14 @@ public class SystemEntitlementsModelMapper : IMapper<SystemEntitlements, SystemE
 {
   private readonly IMapper<RegionInfo, CountryModel> _countryModelMapper;
   private readonly IHostEnvironment _hostEnvironment;
-  private readonly IClock _clock;
   private readonly IMapper<DateTimeZone, TimeZoneModel> _timeZoneModelMapper;
 
   public SystemEntitlementsModelMapper(
     IHostEnvironment hostEnvironment,
-    IClock clock,
     IMapper<RegionInfo, CountryModel> countryModelMapper,
     IMapper<DateTimeZone, TimeZoneModel> timeZoneModelMapper)
   {
     _hostEnvironment = hostEnvironment ?? throw new ArgumentNullException(nameof(hostEnvironment));
-    _clock = clock ?? throw new ArgumentNullException(nameof(clock));
     _countryModelMapper = countryModelMapper ?? throw new ArgumentNullException(nameof(countryModelMapper));
     _timeZoneModelMapper = timeZoneModelMapper ?? throw new ArgumentNullException(nameof(timeZoneModelMapper));
   }
@@ -31,14 +29,13 @@ public class SystemEntitlementsModelMapper : IMapper<SystemEntitlements, SystemE
     var timeZones = source.Countries
       .SelectMany(x => TzdbDateTimeZoneSource.Default.ZoneLocations?.Where(z => string.Equals(x.TwoLetterISORegionName, z.CountryCode, StringComparison.OrdinalIgnoreCase)) ?? [])
       .Select(x => DateTimeZoneProviders.Tzdb[x.ZoneId])
-      .OrderBy(x => x.GetUtcOffset(_clock.GetCurrentInstant()))
       .ToSeq();
 
     return new SystemEntitlementsModel(
       _hostEnvironment.EnvironmentName,
       source.EnvironmentName.ToString() ?? string.Empty,
       [.. source.Countries.Map(_countryModelMapper.Map)],
-      [.. timeZones.Map(_timeZoneModelMapper.Map)],
+      [.. timeZones.Map(_timeZoneModelMapper.Map).OrderBy(x => x.CurrentOffset)],
       source.MaximumCompanyCount);
   }
 }
--- src/API.Web/Mappers/TimeZoneModelMapper.cs
+++ src/API.Web/Mappers/TimeZoneModelMapper.cs
@@ -7,8 +7,23 @@ namespace Fossa.API.Web.Mappers;
 
 public class TimeZoneModelMapper : IMapper<DateTimeZone, TimeZoneModel>
 {
+  private readonly IClock _clock;
+
+  public TimeZoneModelMapper(
+    IClock clock)
+  {
+    _clock = clock ?? throw new ArgumentNullException(nameof(clock));
+  }
+
   public TimeZoneModel Map(DateTimeZone source)
   {
-    return new TimeZoneModel(source.Id, TzdbDateTimeZoneSource.Default.TzdbToWindowsIds[source.Id]);
+    var currentOffset = source.GetUtcOffset(_clock.GetCurrentInstant()).ToTimeSpan();
+    return new TimeZoneModel(
+      source.Id,
+      TzdbDateTimeZoneSource.Default.TzdbToWindowsIds[source.Id],
+      currentOffset,
+      currentOffset.TotalHours,
+      currentOffset.Hours,
+      currentOffset.Minutes);
   }
 }
