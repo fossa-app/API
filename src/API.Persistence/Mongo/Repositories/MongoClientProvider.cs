5cc860f229a1e1e81d05f9b4e783327074257a02
Tigran TIKSN Torosyan <personal@tiksn.com>
2025-01-05 02:11:33 -0600

    Trace MongoDB Calls

--- src/API.Persistence/Mongo/Repositories/MongoClientProvider.cs
+++ src/API.Persistence/Mongo/Repositories/MongoClientProvider.cs
@@ -3,6 +3,7 @@ using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Driver;
+using MongoDB.Driver.Core.Extensions.DiagnosticSources;
 using TIKSN.Data.Mongo;
 
 namespace Fossa.API.Persistence.Mongo.Repositories;
@@ -16,6 +17,9 @@ public class MongoClientProvider : MongoClientProviderBase
   protected override void ConfigureClientSettings(MongoClientSettings mongoClientSettings)
   {
     base.ConfigureClientSettings(mongoClientSettings);
+
+    var options = new InstrumentationOptions { CaptureCommandText = true };
+    mongoClientSettings.ClusterConfigurator = cb => cb.Subscribe(new DiagnosticsActivityEventSubscriber(options));
 
     BsonSerializer.TryRegisterSerializer(new GuidSerializer(GuidRepresentation.Standard));
   }
