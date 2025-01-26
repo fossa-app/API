8848188b4159dc02aa4e4c8f7145a16415d982da
Tigran TIKSN Torosyan <personal@tiksn.com>
2025-01-25 19:17:27 -0600

    Add Address to Branch for Mongo

---
+++ src/API.Persistence/Mongo/Entities/AddressMongo.cs
@@ -0,0 +1,16 @@
+﻿namespace Fossa.API.Persistence.Mongo.Entities;
+
+public class AddressMongo
+{
+  public string? Line1 { get; set; }
+
+  public string? Line2 { get; set; }
+
+  public string? City { get; set; }
+
+  public string? Subdivision { get; set; }
+
+  public string? PostalCode { get; set; }
+
+  public string? CountryCode { get; set; }
+}
