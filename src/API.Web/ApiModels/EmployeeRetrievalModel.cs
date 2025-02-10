443fe15472c81b8459e0bb68c1b5ffe406305ea0
Tigran TIKSN Torosyan <personal@tiksn.com>
2025-02-09 18:33:42 -0600

    Expose Assigned Branch Id via retrieval APIs

--- src/API.Web/ApiModels/EmployeeRetrievalModel.cs
+++ src/API.Web/ApiModels/EmployeeRetrievalModel.cs
@@ -1,5 +1,5 @@
 ﻿namespace Fossa.API.Web.ApiModels;
 
 public record EmployeeRetrievalModel(
-  long Id, long CompanyId,
+  long Id, long CompanyId, long? AssignedBranchId,
   string FirstName, string LastName, string FullName);
