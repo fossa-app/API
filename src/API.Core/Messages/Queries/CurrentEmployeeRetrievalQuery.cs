1830b47f49fddec3a223c7a0ba1db5b01f4d72dd
Tigran TIKSN Torosyan <personal@tiksn.com>
2025-02-12 01:25:39 -0600

    Implement Current Employee Retrieval Query

---
+++ src/API.Core/Messages/Queries/CurrentEmployeeRetrievalQuery.cs
@@ -0,0 +1,12 @@
+﻿using Fossa.API.Core.Entities;
+
+namespace Fossa.API.Core.Messages.Queries;
+
+public record CurrentEmployeeRetrievalQuery(
+    Guid TenantID,
+    Guid UserID)
+  : EntityTenantQuery<EmployeeEntity, EmployeeId, Guid, EmployeeEntity>(TenantID)
+{
+  public override IEnumerable<EmployeeId> AffectingTenantEntitiesIdentities
+    => [];
+}
---
+++ src/API.Core/Messages/Queries/CurrentEmployeeRetrievalQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using Fossa.API.Core.Entities;
+using Fossa.API.Core.Repositories;
+
+namespace Fossa.API.Core.Messages.Queries;
+
+public class CurrentEmployeeRetrievalQueryHandler : IRequestHandler<CurrentEmployeeRetrievalQuery, EmployeeEntity>
+{
+  private readonly IEmployeeQueryRepository _employeeQueryRepository;
+
+  public CurrentEmployeeRetrievalQueryHandler(
+    IEmployeeQueryRepository employeeQueryRepository)
+  {
+    _employeeQueryRepository = employeeQueryRepository ?? throw new ArgumentNullException(nameof(employeeQueryRepository));
+  }
+
+  public async Task<EmployeeEntity> Handle(
+    CurrentEmployeeRetrievalQuery request,
+    CancellationToken cancellationToken)
+  {
+    return await _employeeQueryRepository.GetByUserIdAsync(
+        request.UserID, cancellationToken)
+      .ConfigureAwait(false);
+  }
+}
