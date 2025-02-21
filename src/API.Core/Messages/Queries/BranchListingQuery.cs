5aeb2223742e7fda978998b3a539c9d2223d3b68
Tigran TIKSN Torosyan <personal@tiksn.com>
2025-02-20 19:08:35 -0600

    Convert pagination into query

---
+++ src/API.Core/Messages/Queries/BranchListingQuery.cs
@@ -0,0 +1,13 @@
+﻿using Fossa.API.Core.Entities;
+
+namespace Fossa.API.Core.Messages.Queries;
+
+public record BranchListingQuery(
+    Seq<BranchId> Ids,
+    Guid TenantID,
+    Guid UserID)
+  : EntityTenantQuery<BranchEntity, BranchId, Guid, Seq<BranchEntity>>(TenantID)
+{
+  public override IEnumerable<BranchId> AffectingTenantEntitiesIdentities
+    => Ids;
+}
