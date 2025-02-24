6ba46ade3005624d840a5ae0ac4423956503d5ef
Tigran TIKSN Torosyan <personal@tiksn.com>
2025-02-23 22:39:03 -0600

    Implement Branch Listing Query Handler

---
+++ src/API.Core/Messages/Queries/BranchListingQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using Fossa.API.Core.Entities;
+using Fossa.API.Core.Repositories;
+
+namespace Fossa.API.Core.Messages.Queries;
+
+public class BranchListingQueryHandler : IRequestHandler<BranchListingQuery, Seq<BranchEntity>>
+{
+  private readonly IBranchQueryRepository _branchQueryRepository;
+
+  public BranchListingQueryHandler(IBranchQueryRepository branchQueryRepository)
+  {
+    _branchQueryRepository =
+      branchQueryRepository ?? throw new ArgumentNullException(nameof(branchQueryRepository));
+  }
+
+  public async Task<Seq<BranchEntity>> Handle(
+    BranchListingQuery request,
+    CancellationToken cancellationToken)
+  {
+    var branches = await _branchQueryRepository.ListAsync(request.Ids, cancellationToken).ConfigureAwait(false);
+    return branches.ToSeq();
+  }
+}
