c5c07788d6378533436b242c2eec48fe5b9a663f
Tigran TIKSN Torosyan <personal@tiksn.com>
2025-03-03 22:50:26 -0600

    Implement Employee Query Endpoint

---
+++ src/API.Core/Messages/Queries/EmployeeListingQuery.cs
@@ -0,0 +1,13 @@
+﻿using Fossa.API.Core.Entities;
+
+namespace Fossa.API.Core.Messages.Queries;
+
+public record EmployeeListingQuery(
+    Seq<EmployeeId> Ids,
+    Guid TenantID,
+    Guid UserID)
+  : EntityTenantQuery<EmployeeEntity, EmployeeId, Guid, Seq<EmployeeEntity>>(TenantID)
+{
+  public override IEnumerable<EmployeeId> AffectingTenantEntitiesIdentities
+    => Ids;
+}
