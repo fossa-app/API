15974863d7b3a7a65b6b7c64ebaf5361f36e3e87
Tigran TIKSN Torosyan <personal@tiksn.com>
2025-04-02 00:42:37 -0500

    Create command and query for company

---
+++ src/API.Web/Messages/Commands/CompanyModificationApiCommand.cs
@@ -0,0 +1,7 @@
+﻿using Fossa.API.Core.Messages.Commands;
+
+namespace Fossa.API.Web.Messages.Commands;
+
+public record CompanyModificationApiCommand(
+  string? Name,
+  string? CountryCode) : ICommand;
