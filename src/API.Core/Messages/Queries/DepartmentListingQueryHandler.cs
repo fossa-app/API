27d2d49341e64382601f42fc639411045bd3923d
Tigran TIKSN Torosyan <personal@tiksn.com>
2025-04-17 01:52:28 -0500

    Command and Query Handlers

---
+++ src/API.Core/Messages/Queries/DepartmentListingQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using Fossa.API.Core.Entities;
+using Fossa.API.Core.Repositories;
+
+namespace Fossa.API.Core.Messages.Queries;
+
+public class DepartmentListingQueryHandler : IRequestHandler<DepartmentListingQuery, Seq<DepartmentEntity>>
+{
+  private readonly IDepartmentQueryRepository _departmentQueryRepository;
+
+  public DepartmentListingQueryHandler(IDepartmentQueryRepository departmentQueryRepository)
+  {
+    _departmentQueryRepository = departmentQueryRepository ?? throw new ArgumentNullException(nameof(departmentQueryRepository));
+  }
+
+  public async Task<Seq<DepartmentEntity>> Handle(
+      DepartmentListingQuery request,
+      CancellationToken cancellationToken)
+  {
+    var departments = await _departmentQueryRepository.ListAsync(request.Ids, cancellationToken).ConfigureAwait(false);
+    return departments.ToSeq();
+  }
+}
---
+++ src/API.Core/Repositories/TenantDepartmentPageQuery.cs
@@ -0,0 +1,8 @@
+﻿using TIKSN.Data;
+
+namespace Fossa.API.Core.Repositories;
+
+public record TenantDepartmentPageQuery(
+    Guid TenantId,
+    string Search,
+    Page Page);
