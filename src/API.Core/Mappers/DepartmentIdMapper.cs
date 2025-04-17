a3c73d093b68f41167c1a67b984d5e8e47fb0c4f
Tigran TIKSN Torosyan <personal@tiksn.com>
2025-04-17 01:19:00 -0500

    Create Department Models

---
+++ src/API.Core/Mappers/DepartmentIdMapper.cs
@@ -0,0 +1,17 @@
+﻿using Fossa.API.Core.Entities;
+using TIKSN.Mapping;
+
+namespace Fossa.API.Core.Mappers;
+
+public class DepartmentIdMapper : IMapper<DepartmentId, long>, IMapper<long, DepartmentId>
+{
+  public long Map(DepartmentId source)
+  {
+    return source.AsPrimitive();
+  }
+
+  public DepartmentId Map(long source)
+  {
+    return new DepartmentId(source);
+  }
+}
---
+++ src/API.Persistence/Mongo/Entities/DepartmentMongoEntity.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using TIKSN.Data;
+
+namespace Fossa.API.Persistence.Mongo.Repositories;
+
+public class DepartmentMongoEntity : IEntity<long>
+{
+  [BsonId]
+  public long ID { get; set; }
+
+  [BsonGuidRepresentation(GuidRepresentation.Standard)]
+  public Guid TenantID { get; set; }
+
+  public string? Name { get; set; }
+
+  public long? ParentDepartmentId { get; set; }
+
+  public long ManagerId { get; set; }
+}
