f9d768b64561483dd6f39e9f65928cdb3cc6051c
Tigran TIKSN Torosyan <personal@tiksn.com>
2025-04-17 03:08:01 -0500

    Implement command validations

---
+++ src/API.Core/Validators/DepartmentDeletionCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Fossa.API.Core.Messages.Commands;
+
+namespace Fossa.API.Core.Validators;
+
+public class DepartmentDeletionCommandValidator : AbstractValidator<DepartmentDeletionCommand>
+{
+  public DepartmentDeletionCommandValidator()
+  {
+    RuleFor(x => x.ID).NotEmpty();
+    RuleFor(x => x.TenantID).NotEmpty();
+    RuleFor(x => x.UserID).NotEmpty();
+  }
+}
