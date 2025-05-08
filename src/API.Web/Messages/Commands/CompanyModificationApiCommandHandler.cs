f44fbfa3c6ec234be466cc60a361168a4bbebc43
Tigran TIKSN Torosyan <personal@tiksn.com>
2025-05-08 13:23:45 -0500

    Create CompanyModificationApiCommandHandler

---
+++ src/API.Web/Messages/Commands/CompanyModificationApiCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using Fossa.API.Core.Entities;
+using Fossa.API.Core.Messages.Commands;
+using Fossa.API.Core.Tenant;
+using TIKSN.Globalization;
+using TIKSN.Mapping;
+
+namespace Fossa.API.Web.Messages.Commands;
+
+public class CompanyModificationApiCommandHandler : ApiMessageHandler<CompanyId, CompanyModificationApiCommand, Unit, CompanyModificationCommand, Unit>
+{
+  private readonly IRegionFactory _regionFactory;
+
+  public CompanyModificationApiCommandHandler(
+    IRegionFactory regionFactory,
+    ISender sender,
+    ITenantIdProvider<Guid> tenantIdProvider,
+    IMapper<CompanyId, long> domainIdentityToDataIdentityMapper,
+    IMapper<long, CompanyId> dataIdentityToDomainIdentityMapper) : base(
+      sender,
+      tenantIdProvider,
+      domainIdentityToDataIdentityMapper,
+      dataIdentityToDomainIdentityMapper)
+  {
+    _regionFactory = regionFactory ?? throw new ArgumentNullException(nameof(regionFactory));
+  }
+
+  protected override Unit MapToApiResponse(Unit domainResponse) => domainResponse;
+
+  protected override CompanyModificationCommand MapToDomainRequest(CompanyModificationApiCommand apiRequest)
+  {
+    var tenantId = _tenantIdProvider.GetTenantId();
+    return new CompanyModificationCommand(
+      tenantId,
+      apiRequest.Name ?? string.Empty,
+      _regionFactory.Create(apiRequest.CountryCode ?? string.Empty));
+  }
+}
