d3fe1ccb622480db69e340bb8b5e4ab48ca91de9
Tigran TIKSN Torosyan <personal@tiksn.com>
2025-05-10 20:27:34 -0500

    Implement Handlers

---
+++ src/API.Web/Messages/Commands/BranchModificationApiCommandHandler.cs
@@ -0,0 +1,51 @@
+﻿using Fossa.API.Core.Entities;
+using Fossa.API.Core.Messages.Commands;
+using Fossa.API.Core.Tenant;
+using Fossa.API.Core.TimeZone;
+using Fossa.API.Core.User;
+using Fossa.API.Web.ApiModels;
+using TIKSN.Mapping;
+
+namespace Fossa.API.Web.Messages.Commands;
+
+public class BranchModificationApiCommandHandler :
+    ApiMessageHandler<BranchId, BranchModificationApiCommand, Unit, BranchModificationCommand, Unit>
+{
+  private readonly IMapper<AddressModel, Address> _addressModelToDomainMapper;
+  private readonly IDateTimeZoneProvider _dateTimeZoneProvider;
+
+  public BranchModificationApiCommandHandler(
+        IDateTimeZoneProvider dateTimeZoneProvider,
+        IMapper<AddressModel, Address> addressModelToDomainMapper,
+        ISender sender,
+        ITenantIdProvider<Guid> tenantIdProvider,
+        IUserIdProvider<Guid> userIdProvider,
+        IMapper<BranchId, long> domainIdentityToDataIdentityMapper,
+        IMapper<long, BranchId> dataIdentityToDomainIdentityMapper) : base(
+            sender,
+            tenantIdProvider,
+            userIdProvider,
+            domainIdentityToDataIdentityMapper,
+            dataIdentityToDomainIdentityMapper)
+  {
+    _dateTimeZoneProvider = dateTimeZoneProvider ?? throw new ArgumentNullException(nameof(dateTimeZoneProvider));
+    _addressModelToDomainMapper = addressModelToDomainMapper ?? throw new ArgumentNullException(nameof(addressModelToDomainMapper));
+  }
+
+  protected override Unit MapToApiResponse(Unit domainResponse) => domainResponse;
+
+  protected override BranchModificationCommand MapToDomainRequest(BranchModificationApiCommand apiRequest)
+  {
+    var tenantId = _tenantIdProvider.GetTenantId();
+    var userId = _userIdProvider.GetUserId();
+    var timeZone = _dateTimeZoneProvider.GetDateTimeZoneById(apiRequest.TimeZoneId ?? string.Empty);
+
+    return new BranchModificationCommand(
+        _dataIdentityToDomainIdentityMapper.Map(apiRequest.Id),
+        tenantId,
+        userId,
+        apiRequest.Name ?? string.Empty,
+        timeZone,
+        Optional(apiRequest.Address).Map(_addressModelToDomainMapper.Map));
+  }
+}
---
+++ src/API.Web/Messages/Queries/BranchRetrievalApiQueryHandler.cs
@@ -0,0 +1,46 @@
+using Fossa.API.Core.Entities;
+using Fossa.API.Core.Messages.Queries;
+using Fossa.API.Core.Tenant;
+using Fossa.API.Core.User;
+using Fossa.API.Web.ApiModels;
+using MediatR;
+using TIKSN.Mapping;
+
+namespace Fossa.API.Web.Messages.Queries;
+
+public class BranchRetrievalApiQueryHandler :
+    ApiMessageHandler<BranchId, BranchRetrievalApiQuery, BranchRetrievalModel, BranchRetrievalQuery, BranchEntity>
+{
+    private readonly IMapper<BranchEntity, BranchRetrievalModel> _domainResponseToApiResponseMapper;
+
+    public BranchRetrievalApiQueryHandler(
+        IMapper<BranchEntity, BranchRetrievalModel> domainResponseToApiResponseMapper,
+        ISender sender,
+        ITenantIdProvider<Guid> tenantIdProvider,
+        IUserIdProvider<Guid> userIdProvider,
+        IMapper<BranchId, long> domainIdentityToDataIdentityMapper,
+        IMapper<long, BranchId> dataIdentityToDomainIdentityMapper) : base(
+            sender,
+            tenantIdProvider,
+            userIdProvider,
+            domainIdentityToDataIdentityMapper,
+            dataIdentityToDomainIdentityMapper)
+    {
+        _domainResponseToApiResponseMapper = domainResponseToApiResponseMapper ?? throw new ArgumentNullException(nameof(domainResponseToApiResponseMapper));
+    }
+
+    protected override BranchRetrievalQuery MapToDomainRequest(BranchRetrievalApiQuery apiRequest)
+    {
+        var tenantId = _tenantIdProvider.GetTenantId();
+        var userId = _userIdProvider.GetUserId();
+        return new BranchRetrievalQuery(
+            _dataIdentityToDomainIdentityMapper.Map(apiRequest.Id),
+            tenantId,
+            userId);
+    }
+
+    protected override BranchRetrievalModel MapToApiResponse(BranchEntity domainResponse)
+    {
+        return _domainResponseToApiResponseMapper.Map(domainResponse);
+    }
+}
