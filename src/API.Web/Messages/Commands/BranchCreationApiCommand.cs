3c3ca1d3ec6cff3aab56a9cb0a85b2130eb9076d
Tigran TIKSN Torosyan <personal@tiksn.com>
2025-05-10 20:26:45 -0500

    Implement branch commands and queries

---
+++ src/API.Web/Messages/Commands/BranchCreationApiCommand.cs
@@ -0,0 +1,9 @@
+﻿using Fossa.API.Core.Messages.Commands;
+using Fossa.API.Web.ApiModels;
+
+namespace Fossa.API.Web.Messages.Commands;
+
+public record BranchCreationApiCommand(
+    string? Name,
+    string? TimeZoneId,
+    AddressModel? Address) : ICommand;
---
+++ src/API.Web/Messages/Commands/BranchModificationApiCommand.cs
@@ -0,0 +1,10 @@
+﻿using Fossa.API.Core.Messages.Commands;
+using Fossa.API.Web.ApiModels;
+
+namespace Fossa.API.Web.Messages.Commands;
+
+public record BranchModificationApiCommand(
+    long Id,
+    string? Name,
+    string? TimeZoneId,
+    AddressModel? Address) : ICommand;
