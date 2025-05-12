02a63712ab8c8194cc683515701316feb56e5053
Tigran TIKSN Torosyan <personal@tiksn.com>
2025-05-12 00:05:54 -0500

    Fix Search query for functional tests

--- tests/API.FunctionalTests/Repositories/EmployeeMongoEasyRepository.cs
+++ tests/API.FunctionalTests/Repositories/EmployeeMongoEasyRepository.cs
@@ -52,7 +52,13 @@ public class EmployeeMongoEasyRepository
 
   public Task<PageResult<EmployeeMongoEntity>> PageAsync(TenantEmployeePageQuery pageQuery, CancellationToken cancellationToken)
   {
-    var allItems = EasyStore.Entities.Values.Where(x => x.TenantID == pageQuery.TenantId).ToList();
+    var allItems = EasyStore.Entities.Values
+      .Where(x => x.TenantID == pageQuery.TenantId)
+      .Where(x => string.IsNullOrEmpty(pageQuery.Search)
+      || x.FirstName?.Contains(pageQuery.Search, StringComparison.OrdinalIgnoreCase) == true
+      || x.LastName?.Contains(pageQuery.Search, StringComparison.OrdinalIgnoreCase) == true
+      || x.FullName?.Contains(pageQuery.Search, StringComparison.OrdinalIgnoreCase) == true)
+      .ToList();
 
     var pageItems = allItems.Skip((pageQuery.Page.Number - 1) * pageQuery.Page.Size).Take(pageQuery.Page.Size).ToList();
 
