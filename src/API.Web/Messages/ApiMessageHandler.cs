968a4c3fb0bb729c77d691ef6d5447c979cfc603
Tigran TIKSN Torosyan <personal@tiksn.com>
2025-05-15 23:26:11 -0500

    Create base ApiMessageHandler

--- src/API.Web/Messages/ApiMessageHandler.cs
+++ src/API.Web/Messages/ApiMessageHandler.cs
@@ -4,9 +4,8 @@ using TIKSN.Mapping;
 
 namespace Fossa.API.Web.Messages;
 
-public abstract class ApiMessageHandler<TEntityIdentity, TApiRequest, TApiResponse, TDomainRequest, TDomainResponse> : IRequestHandler<TApiRequest, TApiResponse>
+public abstract class ApiMessageHandler<TEntityIdentity, TApiRequest, TApiResponse> : IRequestHandler<TApiRequest, TApiResponse>
   where TApiRequest : IRequest<TApiResponse>
-  where TDomainRequest : IRequest<TDomainResponse>
 {
   protected readonly IMapper<long, TEntityIdentity> _dataIdentityToDomainIdentityMapper;
   protected readonly IMapper<TEntityIdentity, long> _domainIdentityToDataIdentityMapper;
@@ -28,7 +27,25 @@ public abstract class ApiMessageHandler<TEntityIdentity, TApiRequest, TApiRespon
     _dataIdentityToDomainIdentityMapper = dataIdentityToDomainIdentityMapper ?? throw new ArgumentNullException(nameof(dataIdentityToDomainIdentityMapper));
   }
 
-  public async Task<TApiResponse> Handle(TApiRequest request, CancellationToken cancellationToken)
+  public abstract Task<TApiResponse> Handle(TApiRequest request, CancellationToken cancellationToken);
+}
+
+public abstract class ApiMessageHandler<TEntityIdentity, TApiRequest, TApiResponse, TDomainRequest, TDomainResponse>
+  : ApiMessageHandler<TEntityIdentity, TApiRequest, TApiResponse>
+  where TApiRequest : IRequest<TApiResponse>
+  where TDomainRequest : IRequest<TDomainResponse>
+{
+  protected ApiMessageHandler(
+    ISender sender,
+    ITenantIdProvider<Guid> tenantIdProvider,
+    IUserIdProvider<Guid> userIdProvider,
+    IMapper<TEntityIdentity, long> domainIdentityToDataIdentityMapper,
+    IMapper<long, TEntityIdentity> dataIdentityToDomainIdentityMapper)
+    : base(sender, tenantIdProvider, userIdProvider, domainIdentityToDataIdentityMapper, dataIdentityToDomainIdentityMapper)
+  {
+  }
+
+  public override async Task<TApiResponse> Handle(TApiRequest request, CancellationToken cancellationToken)
   {
     if (request is null)
     {
@@ -50,32 +67,23 @@ public abstract class ApiMessageHandler<TEntityIdentity, TApiRequest, TApiRespon
   protected abstract TDomainRequest MapToDomainRequest(TApiRequest apiRequest);
 }
 
-public abstract class ApiMessageHandler<TEntityIdentity, TApiRequest, TApiResponse, TDomainRequest1, TDomainResponse1, TDomainRequest2, TDomainResponse2> : IRequestHandler<TApiRequest, TApiResponse>
+public abstract class ApiMessageHandler<TEntityIdentity, TApiRequest, TApiResponse, TDomainRequest1, TDomainResponse1, TDomainRequest2, TDomainResponse2>
+  : ApiMessageHandler<TEntityIdentity, TApiRequest, TApiResponse>
   where TApiRequest : IRequest<TApiResponse>
   where TDomainRequest1 : IRequest<TDomainResponse1>
   where TDomainRequest2 : IRequest<TDomainResponse2>
 {
-  protected readonly IMapper<long, TEntityIdentity> _dataIdentityToDomainIdentityMapper;
-  protected readonly IMapper<TEntityIdentity, long> _domainIdentityToDataIdentityMapper;
-  protected readonly ISender _sender;
-  protected readonly ITenantIdProvider<Guid> _tenantIdProvider;
-  protected readonly IUserIdProvider<Guid> _userIdProvider;
-
   protected ApiMessageHandler(
     ISender sender,
     ITenantIdProvider<Guid> tenantIdProvider,
     IUserIdProvider<Guid> userIdProvider,
     IMapper<TEntityIdentity, long> domainIdentityToDataIdentityMapper,
     IMapper<long, TEntityIdentity> dataIdentityToDomainIdentityMapper)
+  : base(sender, tenantIdProvider, userIdProvider, domainIdentityToDataIdentityMapper, dataIdentityToDomainIdentityMapper)
   {
-    _sender = sender ?? throw new ArgumentNullException(nameof(sender));
-    _tenantIdProvider = tenantIdProvider ?? throw new ArgumentNullException(nameof(tenantIdProvider));
-    _userIdProvider = userIdProvider ?? throw new ArgumentNullException(nameof(userIdProvider));
-    _domainIdentityToDataIdentityMapper = domainIdentityToDataIdentityMapper ?? throw new ArgumentNullException(nameof(domainIdentityToDataIdentityMapper));
-    _dataIdentityToDomainIdentityMapper = dataIdentityToDomainIdentityMapper ?? throw new ArgumentNullException(nameof(dataIdentityToDomainIdentityMapper));
   }
 
-  public Task<TApiResponse> Handle(TApiRequest request, CancellationToken cancellationToken)
+  public override Task<TApiResponse> Handle(TApiRequest request, CancellationToken cancellationToken)
   {
     if (request is null)
     {
