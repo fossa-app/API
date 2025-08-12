de26f9e93ed6b1d4369ed3c3384d917cd924f626
Tigran TIKSN Torosyan <personal@tiksn.com>
2025-08-12 16:52:05 -0500

    Record system license metrics

---
+++ src/API.Core/Telemetry/ISystemLicenseMetricsRecorder.cs
@@ -0,0 +1,13 @@
+﻿using Fossa.Licensing;
+using TIKSN.Licensing;
+
+namespace Fossa.API.Core.Telemetry;
+
+public interface ISystemLicenseMetricsRecorder
+{
+  /// <summary>
+  /// Records the system license metrics.
+  /// </summary>
+  /// <param name="license">The license to record.</param>
+  void Record(Validation<Error, License<SystemEntitlements>> license);
+}
