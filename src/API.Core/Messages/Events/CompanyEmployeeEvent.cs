98a48f6210b8f99417eae3f8b93661e4c2fc3f67
Tigran TIKSN Torosyan <personal@tiksn.com>
2026-02-07 21:48:48 -0600

    Add CompanyEmployeeEvent, CompanyEvent, TenantEvent, and TenantUserEvent records

---
+++ src/API.Core/Messages/Events/CompanyEmployeeEvent.cs
@@ -0,0 +1,17 @@
+﻿using Fossa.API.Core.Entities;
+
+namespace Fossa.API.Core.Messages.Events;
+
+public abstract record CompanyEmployeeEvent<TUserIdentity, TTenantIdentity> : TenantUserEvent<TUserIdentity, TTenantIdentity>, ICompanyEmployeeEvent<TUserIdentity, TTenantIdentity>
+  where TUserIdentity : IEquatable<TUserIdentity>
+  where TTenantIdentity : IEquatable<TTenantIdentity>
+{
+  protected CompanyEmployeeEvent(TTenantIdentity tenantID, CompanyId companyId, TUserIdentity userID, EmployeeId employeeId) : base(tenantID, userID)
+  {
+    CompanyId = companyId;
+    EmployeeId = employeeId;
+  }
+
+  public CompanyId CompanyId { get; }
+  public EmployeeId EmployeeId { get; }
+}
---
+++ src/API.Core/Messages/Events/TenantUserEvent.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using TIKSN.Integration.Messages.Events;
+
+namespace Fossa.API.Core.Messages.Events;
+
+public abstract record TenantUserEvent<TUserIdentity, TTenantIdentity> : TenantEvent<TTenantIdentity>, IUserEvent<TUserIdentity>
+  where TUserIdentity : IEquatable<TUserIdentity>
+  where TTenantIdentity : IEquatable<TTenantIdentity>
+{
+  protected TenantUserEvent(TTenantIdentity tenantID, TUserIdentity userID) : base(tenantID)
+  {
+    UserID = userID;
+  }
+
+  public TUserIdentity UserID { get; }
+}
