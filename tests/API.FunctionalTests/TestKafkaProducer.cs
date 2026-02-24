dffa39ceb61aa999ba952e3b0c34d152b0a0e33b
Tigran TIKSN Torosyan <personal@tiksn.com>
2026-02-24 14:27:14 -0600

    feat: Introduce functional test infrastructure with a custom web factory and mock Kafka producer provider.

---
+++ tests/API.FunctionalTests/TestKafkaProducer.cs
@@ -0,0 +1,92 @@
+﻿using Confluent.Kafka;
+
+namespace Fossa.API.FunctionalTests;
+
+#pragma warning disable S3881 // "IDisposable" should be implemented correctly
+public class TestKafkaProducer : IProducer<string?, byte[]>
+#pragma warning restore S3881 // "IDisposable" should be implemented correctly
+{
+  public Handle Handle => throw new NotImplementedException();
+
+  public string Name => throw new NotImplementedException();
+
+  public void AbortTransaction(TimeSpan timeout)
+  {
+    throw new NotImplementedException();
+  }
+
+  public void AbortTransaction()
+  {
+    throw new NotImplementedException();
+  }
+
+  public int AddBrokers(string brokers)
+  {
+    throw new NotImplementedException();
+  }
+
+  public void BeginTransaction()
+  {
+    throw new NotImplementedException();
+  }
+
+  public void CommitTransaction(TimeSpan timeout)
+  {
+    throw new NotImplementedException();
+  }
+
+  public void CommitTransaction()
+  {
+    throw new NotImplementedException();
+  }
+
+  public void Dispose()
+  {
+    throw new NotImplementedException();
+  }
+
+  public int Flush(TimeSpan timeout)
+  {
+    throw new NotImplementedException();
+  }
+
+  public void Flush(CancellationToken cancellationToken = default)
+  {
+    throw new NotImplementedException();
+  }
+
+  public void InitTransactions(TimeSpan timeout)
+  {
+    throw new NotImplementedException();
+  }
+
+  public int Poll(TimeSpan timeout)
+  {
+    throw new NotImplementedException();
+  }
+
+  public void Produce(string topic, Message<string?, byte[]> message, Action<DeliveryReport<string?, byte[]>>? deliveryHandler = null)
+  {
+    throw new NotImplementedException();
+  }
+
+  public void Produce(TopicPartition topicPartition, Message<string?, byte[]> message, Action<DeliveryReport<string?, byte[]>>? deliveryHandler = null)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<DeliveryResult<string?, byte[]>> ProduceAsync(string topic, Message<string?, byte[]> message, CancellationToken cancellationToken = default)
+  {
+    throw new NotImplementedException();
+  }
+
+  public Task<DeliveryResult<string?, byte[]>> ProduceAsync(TopicPartition topicPartition, Message<string?, byte[]> message, CancellationToken cancellationToken = default)
+  {
+    throw new NotImplementedException();
+  }
+
+  public void SendOffsetsToTransaction(IEnumerable<TopicPartitionOffset> offsets, IConsumerGroupMetadata groupMetadata, TimeSpan timeout)
+  {
+    throw new NotImplementedException();
+  }
+}
---
+++ tests/API.FunctionalTests/TestProducerProvider.cs
@@ -0,0 +1,12 @@
+﻿using Confluent.Kafka;
+using Fossa.Messaging;
+
+namespace Fossa.API.FunctionalTests;
+
+public class TestProducerProvider : IProducerProvider
+{
+  public IProducer<string?, byte[]> GetProducer()
+  {
+    return new TestKafkaProducer();
+  }
+}
